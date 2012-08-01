a9659e89246517b955487a2871782c0589906a76
Brent Townshend <bst@tc.com>
2012-07-31 23:34:21 -0700

    Use velocity instead of delta+speed

--- Common/inittgthypo.m
+++ Common/inittgthypo.m
@@ -7,11 +7,11 @@
 %	.tnum - target number(s)
 %	.like - likelihood
 function snap=inittgthypo(snap)
-h=struct('id',{},'pos',{},'tnum',{},'like',{},'entrytime',{},'lasttime',{},'area',{},'delta',{},'speed',{},'heading',{},'orientation',{},'minoraxislength',{},'majoraxislength',{});
+h=struct('id',{},'pos',{},'tnum',{},'like',{},'entrytime',{},'lasttime',{},'area',{},'velocity',{},'heading',{},'orientation',{},'minoraxislength',{},'majoraxislength',{});
 tgts=snap.tgts;
 for i=1:length(tgts)
   if tgts(i).nunique>10
-    h=[h,struct('pos',tgts(i).pos,'tnum',i,'like',1,'id',1,'entrytime',snap.when,'lasttime',snap.when,'area',nan,'delta',nan,'speed',nan,'heading',nan,'orientation',nan,'minoraxislength',nan,'majoraxislength',nan)];
+    h=[h,struct('pos',tgts(i).pos,'tnum',i,'like',1,'id',length(h)+1,'entrytime',snap.when,'lasttime',snap.when,'area',nan,'velocity',[nan,nan],'heading',nan,'orientation',nan,'minoraxislength',nan,'majoraxislength',nan)];
     fprintf('Initializing hypo %d to position of target %d - (%.1f, %.1f)\n', length(h), i, tgts(i).pos);
   else
     fprintf('Initializing hypo: skip target %d - nunique=%d\n', i, tgts(i).nunique);
