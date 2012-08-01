c3e7a66315cfc1cdc348ebdf7c7b0a475ef94a16
Brent Townshend <bst@tc.com>
2012-07-31 23:35:26 -0700

    Moved layout, number of pixels into p structure

--- Common/createrays.m
+++ Common/createrays.m
@@ -4,18 +4,19 @@
 % Returns
 %   r.rays{c}(x,y) - image of region with each pixel indicating LED# that ray from this camera hits after passing through this pixel, or 0 for none
 %   r.raylines{c,l} - path (in integer pixel coordinates) of ray from camera c to led l, stepping by 1 pixel in fastest changing direction
-function r=createrays(layout,pixels)
-imap=setupimap(layout,pixels);   % Setup mapping between pixel and physical coords
-cpos=m2pix(imap,layout.cpos);
-lpos=m2pix(imap,layout.lpos);
+function r=createrays(p)
+pixels=p.analysisparams.npixels;
+imap=setupimap(p.layout,pixels);   % Setup mapping between pixel and physical coords
+cpos=m2pix(imap,p.layout.cpos);
+lpos=m2pix(imap,p.layout.lpos);
 
 % Create rays (image with each pixel indicating the highest LED number that the ray is directed to)
 % Also create raylines{c,l}(i,2) - coords of points along ray from camera c to led l
 raylines={};
-for c=1:size(layout.cpos,1)
+for c=1:size(p.layout.cpos,1)
   fprintf('Computing rays from camera %d\n',c);
   rays=zeros(imap.isize(1),imap.isize(2));
-  for l=1:size(layout.lpos,1)
+  for l=1:size(lpos,1)
     raylines{c,l}=zeros(0,2);
     cp=cpos(c,:);
     lp=lpos(l,:);
