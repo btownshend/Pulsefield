e2fdeb4d795b388f8d2d4d50884dbdfc85eee342
Brent Townshend <bst@tc.com>
2012-08-13 20:00:03 -0700

    Faster updates of all LEDs

---
+++ LED/ledspeed.m
@@ -0,0 +1,45 @@
+% Check speed of full changes of LEDS
+s1=arduino_ip;
+sync(s1);
+nled=numled();
+level=127;
+nsent=0;
+delay=0.0;
+color=zeros(nled,3);
+% Pattern
+color(:,1)=mod((1:size(color,1))*1,127);
+color(:,2)=mod((1:size(color,1))*2,127);
+color(:,3)=mod((1:size(color,1))*3,127);
+counter=-1;
+getnumrcvd(s1);   % Clear Arduino receipt count
+nrpt=200;
+tic;
+for rpt=1:nrpt
+  fprintf('Repeat %d/%d Num Sent=%d\n',rpt,nrpt,nsent);
+  n1=now;
+  c1=setallleds(s1,color,0);
+  c2=show(s1,delay,0);
+  [counter,c3]=syncsend(s1,0);
+  c=[c1,c2,c3];
+  awrite(s1,c);
+  nsent=nsent+length(c);
+  fprintf('sent %d bytes, wait...',length(c));
+  n2=now;
+  syncwait(s1,counter,5);
+  n3=now;
+  fprintf('%.0f,%.0f msec\n',[n2-n1,n3-n2]*24*3600*1e3);
+
+  % Shift the colors
+  color=color([end,1:end-1],:);
+  color=randi(127,size(color));
+end
+elapsed=toc;
+
+nrcvd=getnumrcvd(s1);
+nsent=nsent+1;  % For getnumrcvd() cmd
+fprintf('Sent %d, Arduino received %d\n', nsent, nrcvd);
+if nsent~=nrcvd
+  fprintf('**** Dropped %d bytes\n', nsent-nrcvd);
+end
+
+fprintf('Changed LEDs %d times in %.2f seconds = %.2f changes/second\n', nrpt, elapsed, nrpt/elapsed);
---
+++ LED/setallleds.m
@@ -0,0 +1,45 @@
+% Turn on LEDs
+% setallleds(s1,firstindex,color[],execute,unmapped)
+% s1-descriptor
+% color(N,3) - colors for each of numled() leds
+% execute - true to send to Arduino now (default true)
+% unmapped - true to NOT map to ordered LEDs (default false)
+function cmd=setallleds(s1,color,execute,unmapped)
+cmd=[];
+if nargin<3
+  execute=1;
+end
+
+% Map to correct physical led number
+if nargin>=4 && unmapped
+  fprintf('Unmapped\n');
+  posmap=0:size(color,1)-1;
+else
+  if size(color,1)~=numled()
+    fprintf('setallleds with %d colors instead of expected %d\n', size(color,1),numled());
+    return;
+  end
+  posmap=[480+(159:-1:0),320+(0:50),0+(159:-1:0),160+(0:159)];
+end
+
+physleds=max(posmap)+1;
+% Remap colors
+mcolor=zeros(physleds,3,'uint8');
+mcolor(posmap+1,:)= bitset(color,8);
+
+nsent=0;
+while nsent<size(mcolor,1)
+  send=min(size(mcolor,1)-nsent,255);
+  pcmd=zeros(1,4+3*send,'uint8');
+  pcmd(1)='F';
+  pcmd(2)=bitand(nsent,255);
+  pcmd(3)=bitshift(nsent,-8);
+  pcmd(4)=send;
+  ind=1:3:send*3-2;
+  % Reorder as GRB
+  pcmd(ind+4)=mcolor(nsent+1:nsent+send,2);
+  pcmd(ind+5)=mcolor(nsent+1:nsent+send,1);
+  pcmd(ind+6)=mcolor(nsent+1:nsent+send,3);
+  cmd=[cmd,pcmd];
+  nsent=nsent+send;
+end
