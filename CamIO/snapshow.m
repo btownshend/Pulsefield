5abbfecd4409586c6c4b484b5082ef8d62b64cc4
Brent Townshend <bst@tc.com>
2012-10-01 09:46:14 -0700

    New file

---
+++ CamIO/snapshow.m
@@ -0,0 +1,8 @@
+function snapshow(snapshot)
+  setfig('snapshot');
+  for i=1:length(snapshot)
+    subplot(2,ceil(length(snapshot)/2),i)
+    imshow(snapshot(i).im);
+    title(datestr(snapshot(i).when));
+  end
+end
