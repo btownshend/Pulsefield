a35b08e2df3bf38a0c5cdae92b70e60753e9b1fe
Brent Townshend <bst@tc.com>
2012-10-03 00:53:54 -0700

    Locate groups

---
+++ RealTime/findgroups.m
@@ -0,0 +1,13 @@
+% findgroups - find groups in a set of hypo
+function snap=findgroups(snap)
+for i=1:length(snap.hypo)
+  % Compute groupid -- lowest id in group, or 0 if not in a group
+  sametnum=find(snap.hypo(i).tnum==[snap.hypo.tnum]);
+  if length(sametnum)<=1  % Could be one if the hypo doesn't have a matching tgt (such as when exitting)
+    snap.hypo(i).groupid=0;
+    snap.hypo(i).groupsize=1;
+  else
+    snap.hypo(i).groupid=min([snap.hypo(sametnum).id]);
+    snap.hypo(i).groupsize=length(sametnum);
+  end
+end
