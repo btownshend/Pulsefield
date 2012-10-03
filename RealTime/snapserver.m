9ccd27abbd286719a584a354b74f7feaeb1aed92
Brent Townshend <bst@tc.com>
2012-10-02 20:45:28 -0700

    Changed to 30s period Change label to green when running Send /snapshot message to TO when triggerred Respond to incoming /snapshot/trigger message

--- RealTime/snapserver.m
+++ RealTime/snapserver.m
@@ -17,10 +17,12 @@ function snapserver
   [~,myport]=getsubsysaddr('SN');
   fprintf('Instructing Matlab processor to use port %d to send us msgs\n', myport);
   oscmsgout('MPO','/pf/dest/add/port',{myport,'SN'},debug);
+  oscmsgout('TO','/snapshot/label/color',{'green'});
 
-  period=5;
+  period=30;
   lastsnap=0;
   npeople=0;
+  trigger=false;
   
   while true
     % Receive any OSC messages
@@ -39,6 +41,8 @@ function snapserver
           fprintf('Last exit - stopping snapshots\n');
         end
         npeople=m.data{1};
+      elseif strcmp(m.path,'/snapshot/trigger') && m.data{1}==1
+        trigger=true;
       else
         if ~ismember(m.path,ignores)
           fprintf('Snapserver: Unhandled OSC message from %s: %s - ignoring from now on.\n', m.src, m.path);
@@ -46,9 +50,12 @@ function snapserver
         end
       end
     end
-    if (now-lastsnap)*3600*24>=period  && npeople>0
+    if trigger || ((now-lastsnap)*3600*24>=period  && npeople>0)
+      oscmsgout('TO','/snapshot',{int32(1)});
       snapshot(p,true);
+      oscmsgout('TO','/snapshot',{int32(0)});
       lastsnap=now;
+      trigger=false;
     end
   end
 end
