56810237ad54756c129054da8feb83ea5bc54540
Brent Townshend <bst@tc.com>
2013-08-12 13:07:07 -0400

    Javadoc initialized for other modules (stubs)

--- Video/Tracker/src/Scale.java
+++ Video/Tracker/src/Scale.java
@@ -19,12 +19,20 @@ class ScaleType {
 	}
 }
 
+/** A musical scale
+ * @author Brent Townshend
+ *
+ */
 public class Scale {
 	ScaleType stype;
 	String key;
 	int notes[];  // Midi notes for center octave of scale
 	
-	Scale(String scaleName,String keyname) {
+	/** Create a new scale
+	 * @param scaleName name of scale (e.g. "Major")
+	 * @param keyname key of scale (e.g. "C#" )
+	 */
+	public Scale(String scaleName,String keyname) {
 		final String keys[]={"C","C#","Db","D","D#","Eb","E","F","F#","Gb","G","G#","Ab","A","A#","Bb","B","B#","Cb"};
 		final int keyv[]={60,61,61,62,63,63,64,65,66,66,67,68,68,69,70,70,71,72,72};
 		ScaleType scale=ScaleType.find(scaleName);
@@ -52,13 +60,29 @@ public class Scale {
 		PApplet.println("");
 	}
 
+	/** Get number of notes in scale
+	 * @return
+	 */
 	public int length() {
 		return notes.length;
 	}
 	
+	/** Get the MIDI pitch for the i-th note in the scale
+	 * @param i
+	 * @return
+	 */
 	public int get(int i) {
 		return notes[i];
 	}
+	
+	/** Map a value to a MIDI note within scale 
+	 * @param val value to map
+	 * @param minval minimum of range
+	 * @param maxval maximum of range
+	 * @param offset increase final pitch by offset steps in scale 
+	 * @param noctave number of octaves (pitches cover center-most octaves of scale)
+	 * @return MIDI note
+	 */
 	public int map2note(float val, float minval, float maxval, int offset, int noctave) {
 		int notenum=(int)((val-minval)/(maxval-minval)*notes.length*noctave)+offset;
 		int pitch=notes[notenum%notes.length];
