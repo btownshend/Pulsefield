2bff0a46577c63b56f88eada5a10047ab50ec1a9
Brent Townshend <bst@tc.com>
2013-08-17 13:01:32 -0700

    Modified handling of urlconfig.txt in FrontEnd to be more robust

--- FrontEnd/urlconfig.cc
+++ FrontEnd/urlconfig.cc
@@ -14,30 +14,31 @@ URLConfig::URLConfig(const char *configFile) {
 	exit(1);
     }
     for (int i=0;i<MAXURLS;i++) {
-	const unsigned int MAXLEN=50;
+	const unsigned int MAXLEN=2000;
 	char tmp_ident[MAXLEN+1];
 	char tmp_host[MAXLEN+1];
-	int nread=fscanf(fd,"%50[^,],%50[^,],%d\n",tmp_ident,tmp_host,&ports[i]);
-	if (strlen(tmp_ident)>=MAXLEN || strlen(tmp_host)>=MAXLEN) {
-	    fprintf(stderr,"Data field too long in %s near line %d\n", configFile, i+1);
-	    exit(1);
-	}
+	while (true) {
+	    int nread=fscanf(fd,"%50[^,],%50[^,],%d\n",tmp_ident,tmp_host,&ports[i]);
+	    if (strlen(tmp_ident)>=MAXLEN || strlen(tmp_host)>=MAXLEN) {
+		fprintf(stderr,"Data field too long in %s near line %d\n", configFile, i+1);
+		exit(1);
+	    }
 
-	if  (nread<0) {
-	    nurl=i;
-	    return;
-	}
-	if (nread!=3) {
+	    if  (nread<0) {
+		nurl=i;
+		return;
+	    }
+	    if (nread==3)
+		break;
+
 	    fprintf(stderr,"Format error near line %d while reading from %s, nread=%d\n", i+1,configFile,nread);
-	    exit(1);
 	}
-
 	idents[i]=new char[strlen(tmp_ident)+1];
 	strcpy(idents[i],tmp_ident);
 	hosts[i]=new char[strlen(tmp_host)+1];
 	strcpy(hosts[i],tmp_host);
 
-	// printf("Set %s to %s:%d\n", idents[i], hosts[i], ports[i]);
+	printf("Set %s to %s:%d\n", idents[i], hosts[i], ports[i]);
     }
     nurl=MAXURLS;
 }
