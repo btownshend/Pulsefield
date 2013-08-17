c3d8a50963c0b7c7fb739177ef3f74a1157f9a5c
Brent Townshend <bst@tc.com>
2013-08-17 13:18:54 -0700

    DDR Improvements

--- Video/Tracker/src/VisualizerDDR.java
+++ Video/Tracker/src/VisualizerDDR.java
@@ -2,7 +2,6 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PImage;
@@ -14,11 +13,14 @@ class Dancer {
 	final float DAMPING=.05f;
 	PVector neutral;
 	PVector current;
+	int score;
+
 	Dancer(PVector pos) {
 		neutral=new PVector();
 		neutral.x=pos.x; neutral.y=pos.y;
 		current=new PVector();
 		current.x=pos.x; current.y=pos.y;
+		score=0;
 	}
 	public void update(PVector newpos) {
 		neutral.x=DAMPING*newpos.x+(1-DAMPING)*neutral.x;
@@ -32,33 +34,63 @@ enum Direction {
 	LEFT, RIGHT, UP, DOWN
 }
 
+class Song {
+	String sfdir;
+	String sfname;
+	int clipNumber;
+	int track;
+	Simfile sf;
+	
+	Song(String sfdir, String sfname, int clipNumber)  {
+		this.sfdir=sfdir;
+		this.sfname=sfname;
+		this.clipNumber=clipNumber;
+		track=91;
+		sf=null;
+	}
+	
+	Simfile getSimfile() {
+		if (sf==null) {
+			sf=new Simfile();
+			try {
+				sf.loadSM(sfdir,sfname);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		return sf;
+	}
+}
+
 // Dance revolution visualizer
 public class VisualizerDDR extends Visualizer {
 	final float MINMOVEDIST=.01f;
+	ArrayList<Song> songs;
 	Simfile sf;
-	PImage banner;
 	long startTime;
 	PImage arrow;
+	Song cursong;
+	final float DOTSIZE=50f;
 
 	HashMap<Integer, Dancer> dancers;
-	Simfile moves[];
 
 	VisualizerDDR(PApplet parent) {
 		super();
 		dancers = new HashMap<Integer, Dancer>();
-		arrow = parent.loadImage("arrow.png");
-		sf = new Simfile();
-		try {
-//			sf.loadSM("/Users/bst/Dropbox/Pulsefield/StepMania/Songs/StepMix 1.0/Impossible Fidelity/","impossible.sm");
-			sf.loadSM("/Users/bst/Dropbox/Pulsefield/StepMania/Songs/Plaguemix Series/Super Trouper","supertrouper.sm");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-//		System.out.println(sf.toString());
-		banner=sf.getBanner(parent);
+		arrow = parent.loadImage("arrow3.png");
+		songs=new ArrayList<Song>();
+		songs.add(new Song("/Users/bst/Dropbox/Pulsefield/StepMania/Songs/StepMix 1.0/Impossible Fidelity/","impossible.sm",0));
+		songs.add(new Song("/Users/bst/Dropbox/Pulsefield/StepMania/Songs/Plaguemix Series/Super Trouper","supertrouper.sm",1));
+		chooseSong();
 	}
 
+	/* Randomly choose a song */
+	void chooseSong() {
+		int songIndex = (int)(Math.random()*songs.size());
+		cursong=songs.get(songIndex);
+		PApplet.println("Chose song "+songIndex+": "+cursong.getSimfile().toString());
+	}
+	
 	public void update(PApplet parent, Positions allpos) {
 		// Update internal state of the dancers
 		for (int id: allpos.positions.keySet()) {
@@ -85,14 +117,15 @@ public class VisualizerDDR extends Visualizer {
 	public void start() {
 		startTime=System.currentTimeMillis();
 		PApplet.println("Starting DDR at "+startTime);
-		Ableton.getInstance().playClip(91,1);
+		chooseSong();
+		Ableton.getInstance().playClip(cursong.track,cursong.clipNumber);
 	}
-	
+
 	public void stop() {
 		PApplet.println("Stopping DDR at "+System.currentTimeMillis());
-		Ableton.getInstance().stopClip(91,1);
+		Ableton.getInstance().stopClip(cursong.track,cursong.clipNumber);
 	}
-	
+
 	public void draw(PApplet parent, Positions p, PVector wsize) {
 		final float leftwidth=150;
 		final float rightwidth=250;
@@ -107,28 +140,29 @@ public class VisualizerDDR extends Visualizer {
 		super.draw(parent, p, wsize);
 
 
-		drawScores(parent,new PVector(leftwidth,wsize.y));
+		drawScores(parent,p,new PVector(leftwidth,wsize.y));
 		parent.translate(leftwidth,0);
 		drawPF(parent,p,new PVector(wsize.x-leftwidth-rightwidth,wsize.y));
 		parent.translate(wsize.x-leftwidth-rightwidth,0);
-		Clip clip=Ableton.getInstance().getClip(91, 1);
-		if (clip!=null)
+		Clip clip=Ableton.getInstance().getClip(cursong.track, cursong.clipNumber);
+		if (clip!=null) {
+			//PApplet.println("Clip at "+clip.position);
 			drawTicker(parent,new PVector(rightwidth-rightmargin,wsize.y),clip.position/Ableton.getInstance().getTempo()*60f);
+		}
 	}
 
 	public void drawPF(PApplet parent, Positions allpos, PVector wsize) {
-		final float DOTSIZE=50f;
 		final float ARROWSIZE=DOTSIZE;
 		final float ARROWDIST=(ARROWSIZE+DOTSIZE)/2;
 
-	
+
 		parent.stroke(255);
 		parent.strokeWeight(1);
 		parent.fill(0);
 		drawBorders(parent,true,wsize);
 		parent.imageMode(PConstants.CENTER);
 		parent.tint(255);
-		
+
 		// Add drawing code here
 		for (int id: dancers.keySet()) {
 			Dancer d=dancers.get(id);
@@ -150,28 +184,43 @@ public class VisualizerDDR extends Visualizer {
 		}
 	}
 
-	public void drawScores(PApplet parent, PVector wsize) {
-		parent.textAlign(PConstants.LEFT,PConstants.CENTER);
-		parent.textSize(16);
+	public void drawScores(PApplet parent, Positions allpos, PVector wsize) {
+		float lineHeight=wsize.y/12;
+
 		parent.stroke(255);
 		parent.fill(255);
 		parent.tint(255);
-		String names[]={"Camille","Brent","Gideon","Natalie","Gabe"};
-		int score[]={47,23450,-3,3200,19000};
-		float lineHeight=wsize.y/12;
-		for (int i=0;i<names.length;i++) {
-			parent.text(names[i],10f,lineHeight*(i+3));
-			parent.text(""+score[i],wsize.x/2,lineHeight*(i+3));
-		}
+		parent.pushMatrix();
+		
+		
 		parent.textSize(24);
 		parent.textAlign(PConstants.CENTER,PConstants.CENTER);
-		parent.text("SCORES",wsize.x/2,lineHeight);
+		parent.translate(0,lineHeight/2);
+		parent.text("SCORES",wsize.x/2,0);
+		parent.translate(0,lineHeight);
+		parent.textAlign(PConstants.LEFT,PConstants.CENTER);
+		parent.textSize(16);
+
+		for (int id: dancers.keySet()) {
+			Dancer d=dancers.get(id);
+			Position p=allpos.get(id);
+			if (p==null)
+				continue;
+			
+			parent.fill(p.getcolor(parent));
+			parent.ellipse(DOTSIZE/2,0,DOTSIZE/2, DOTSIZE/2);
+			parent.fill(255);
+			parent.text(""+d.score,DOTSIZE*1.5f,0);
+			parent.translate(0, lineHeight);
+		}
+		parent.popMatrix();
+
 	}
 
 	public void drawTicker(PApplet parent, PVector wsize, float now) {
-		final float DURATION=2.0f;  // Duration of display top to bottom
-		final float HISTORY=0.5f;    // Amount of past showing
-		ArrayList<NoteData> notes=sf.getNotes(0, now-HISTORY, now-HISTORY+DURATION);
+		final float DURATION=10.0f;  // Duration of display top to bottom
+		final float HISTORY=2f;    // Amount of past showing
+		ArrayList<NoteData> notes=cursong.getSimfile().getNotes(0, now-HISTORY, now-HISTORY+DURATION);
 		//parent.ellipse(wsize.x/2,wsize.y/2,100,100);
 		parent.textAlign(PConstants.CENTER,PConstants.CENTER);
 		parent.textSize(16);
