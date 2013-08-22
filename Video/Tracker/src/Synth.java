b7761c4973b9bb39215e1e45acac9832368958a4
Brent Townshend <bst@tc.com>
2013-08-22 01:45:52 -0700

    Track notes playing in Synth and extend notes that are played again before their endnote.

--- Video/Tracker/src/Synth.java
+++ Video/Tracker/src/Synth.java
@@ -16,17 +16,16 @@ class NoteOff extends TimerTask {
 	int pitch;
 	int channel;
 	int velocity;
-	MidiBus bus;
-	
-	NoteOff(MidiBus bus, int pitch, int velocity, int channel) {
-		this.bus=bus;
+	Synth synth;
+
+	NoteOff(Synth synth, int pitch, int velocity, int channel) {
+		this.synth=synth;
 		this.pitch=pitch;
 		this.velocity=velocity;
 		this.channel=channel;
 	}
 	public void run() {
-		bus.sendNoteOff(channel, pitch, velocity);
-		//System.out.println("Sent note off "+pitch+", vel="+velocity+" , to channel "+channel);
+		synth.endnote(channel, pitch, velocity);
 	}
 }
 
@@ -34,7 +33,7 @@ public class Synth {
 	final String midiPortName="From Tracker";
 	MidiBus myBus;
 	Timer timer;
-	
+	HashMap<Integer,HashMap<Integer,NoteOff>> playing;    // Notes playing indexed by playing.get(channel).get(pitch)
 	HashMap<Integer,MidiProgram> channelmap;
 
 	Synth(PApplet parent) {
@@ -46,15 +45,36 @@ public class Synth {
 
 		myBus = new MidiBus(parent, -1, "From Tracker");
 		timer = new Timer();
+		playing=new HashMap<Integer,HashMap<Integer,NoteOff>>();
 		play(0,64,100,100,1);
 	}
 
 	public void play(int id, int pitch, int velocity, int duration, int channel) {
 		assert(channel>=0 && channel<16);
-		myBus.sendNoteOn(channel,pitch,velocity);
 		long delay=duration*1000/480/4;
-		timer.schedule(new NoteOff(myBus, pitch,0,channel), delay);
+		if (playing.get(channel)==null)
+			playing.put(channel, new HashMap<Integer,NoteOff>());
+		if (playing.get(channel).get(pitch)!=null)
+			playing.get(channel).get(pitch).cancel();
+		// Check again in case there was a race
+		if (playing.get(channel).get(pitch)!=null) {
+			System.out.println("Already playing note "+pitch+" on channel "+channel+", removing pending note-off");
+			playing.get(channel).get(pitch).cancel();
+		} else {
+			myBus.sendNoteOn(channel,pitch,velocity);
+		}
+		NoteOff noteOff=new NoteOff(this, pitch,0,channel);
+		playing.get(channel).put(pitch, noteOff);
+		timer.schedule(noteOff, delay);
 		System.out.println("Sent note "+pitch+", vel="+velocity+" , duration="+delay+"ms to channel "+channel);
+	}
+
+	public void endnote(int channel, int pitch, int velocity) {
+		if (playing.get(channel).get(pitch)==null)
+			System.out.println("Received endnote for note that isn't playing; channel="+channel+", pitch="+pitch);
+		myBus.sendNoteOff(channel, pitch, velocity);
+		playing.get(channel).remove(pitch);
+		System.out.println("Sent note off "+pitch+", vel="+velocity+" , to channel "+channel+", now have "+playing.get(channel).size()+" notes playing on this channel");
 	}
 
 	public MidiProgram getMidiProgam(int ch) {
@@ -64,7 +84,7 @@ public class Synth {
 	public void setCC(int channel, int cc, int value) {
 		myBus.sendControllerChange(channel, cc, value);
 	}
-	
+
 	public boolean handleMessage(OscMessage msg) {
 		//PApplet.println("Synth message: "+msg.toString());
 		String pattern=msg.addrPattern();
