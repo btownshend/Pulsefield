7b1c7390f7e79c6a452ba3c302b099e6d21cb0bf
Brent Townshend <bst@tc.com>
2013-11-09 17:57:49 -0800

    Added minocc, maxocc, minframes args to lsrecvis

--- RealTime/lsrecvis.m
+++ RealTime/lsrecvis.m
@@ -1,4 +1,7 @@
 % List recordings
+function lsrecvis(varargin)
+defaults=struct('minocc',1,'maxocc',9999,'minframes',1);
+args=processargs(defaults,varargin);
 files=dir([pfroot(),'/Recordings/*.mat']);
 for i=1:length(files)
   f=files(i);
@@ -12,6 +15,19 @@ for i=1:length(files)
   end
   if ~isfield(zz,'vis')
     zz.vis=[];
+  end
+  v=regexp(zz.note,'^.* of ([0-9]*) frames at .* with max occupancy of ([0-9]*)$','tokens');
+  if length(v)<1 || length(v{1})~=2
+    fprintf('Unable to parse note\n');
+    if length(v)>=1
+      disp(v{1});
+    end
+  else
+    nframes=str2num(v{1}{1});
+    occ=str2num(v{1}{2});
+    if occ<args.minocc || occ>args.maxocc || nframes<args.minframes
+      continue;
+    end
   end
   fprintf('%s %s %4.1fMB %s\n', f.name, f.date, round(f.bytes/1024/1024), zz.note);
 end
