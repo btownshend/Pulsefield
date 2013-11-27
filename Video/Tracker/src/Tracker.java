99f77c2cd0967455ddb950bf43eef45bb45aaad9
Brent Townshend <bst@tc.com>
2013-11-26 22:16:23 -0800

    Turned off present mode

--- Video/Tracker/src/Tracker.java
+++ Video/Tracker/src/Tracker.java
@@ -11,7 +11,7 @@ public class Tracker extends PApplet {
 	/**
 	 * 
 	 */
-	private static boolean present = true;
+	private static boolean present = false;
 
 	private static final long serialVersionUID = 1L;
 	int tick=0;
