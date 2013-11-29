f74b35a49086a5167a5f0dc6256b0448ad548af0
Brent Townshend <bst@tc.com>
2013-11-29 14:00:00 -0800

    Check for errors in system command

--- CamIO/arecont.m
+++ CamIO/arecont.m
@@ -3,7 +3,10 @@ function p=arecont(id)
 url=sprintf('http://%s/image?res=full&quality=21&doublescan=0&x0=0&x1=9999&y0=0&y1=9999',h);
 cmd=sprintf('DYLD_LIBRARY_PATH=/opt/local/lib;curl -s ''%s'' >/tmp/im.jpg', url)
 p.captstart=now;
-system(cmd);
+[s,r]=system(cmd);
+if s~=0
+  error('Failed execution of command: %s\nError: %s\n',cmd,r);
+end  
 p.captend=now;
 p.im=imread('/tmp/im.jpg');
 p.info=imfinfo('/tmp/im.jpg');
