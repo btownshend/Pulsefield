5a285005075d9569fd8bf755a376af2f611ddf3e
Brent Townshend <bst@tc.com>
2013-11-29 19:05:46 -0800

    Added room outline, clean up

--- Experimental/plotworld.m
+++ Experimental/plotworld.m
@@ -9,6 +9,7 @@ if nargin<2
   end
 end
 
+clf;
 plot3([0,0],[0,0],[0,1],'r-');
 hold on;
 text(0,0,1,'Z');
@@ -31,13 +32,13 @@ for c=1:length(p.camera)
   campos=tw(world,position);
   fprintf('Camera Position: [%.4f,%.4f,%.4f] in grid, [%.4f, %.4f, %.4f] in world\n', position, campos);
   plot3(campos(1),campos(2),campos(3),'gx');
-  plot3([campos(1),campos(1)],[campos(2),campos(2)],[campos(3),0],'b');
-  plot3([campos(1),campos(1)],[campos(2),0],[0,0],'g');
-  plot3([campos(1),0],[0,0],[0,0],'r');
+  %  plot3([campos(1),campos(1)],[campos(2),campos(2)],[campos(3),0],'b');
+  %  plot3([campos(1),campos(1)],[campos(2),0],[0,0],'g');
+  % plot3([campos(1),0],[0,0],[0,0],'r');
   text(campos(1),campos(2),campos(3),sprintf('C%d',cam.id));
   
   % Plot camera box
-  dist=0.2;vfov=90;hfov=130;
+  dist=0.15;vfov=90;hfov=130;
   xwidth=tand(hfov/2)*dist;
   ywidth=tand(vfov/2)*dist;
   positions={[xwidth;ywidth;dist],[-xwidth;ywidth;dist],[-xwidth;-ywidth;dist],[xwidth;-ywidth;dist],[xwidth;ywidth;dist]};
@@ -53,6 +54,12 @@ end
 box on;
 axis vis3d;
 axis equal;
+if isfield(p,'calibration') && nargin<2
+  % Add room outline and set axes to slightly larger than room
+  extra=0.5;
+  plot3(p.layout.active(:,1),p.layout.active(:,2),0*p.layout.active(:,1),'c');
+  axis([min(p.layout.active(:,1))-extra,max(p.layout.active(:,1))+extra,min(p.layout.active(:,2))-extra,max(p.layout.active(:,2))+extra,0,1.5]);
+end
 
 function tpts=tw(world,pts)
 tpts=world.R*pts;
