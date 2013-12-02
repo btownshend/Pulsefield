acf9ef9f7131153697969e9ad46574f8991e6806
Brent Townshend <bst@tc.com>
2013-12-02 02:53:31 -0800

    Improved noisedetect; made consistent with frontend

--- Experimental/noisedetect.m
+++ Experimental/noisedetect.m
@@ -1,78 +1,97 @@
 % Do noisedetection of a frame
-function noisedetect(p,vis,cam)
-setfig('noisedet');clf;
-nplot=5; pnum=1;
+function noisedetect(p,vis,cam,varargin)
+defaults=struct('wsize',[]);
+args=processargs(defaults,varargin);
 
-nm=noisemodel(vis,cam);
+if isempty(args.wsize)
+  args.wsize= p.camera(1).viscache.wsize;
+end
+
+setfig('noisedet');clf;
+nplot=7; pnum=1;
+
+nm=noisemodel(p,vis,cam);
 img=im2double(vis.im{cam});
 diff=img-nm.avg;
-nstd=abs(diff./nm.std);
-%nstd=nstd.^2;
-fprintf('Mean sample level: %.1f/255, mean std: %.1f/255\n',255*mean(nm.avg(:)),255*mean(nm.std(:)));
+nstd=diff.*diff./nm.var;
+fprintf('Mean sample level: %.1f/255, mean std: %.1f/255\n',255*mean(nm.avg(:)),255*mean(sqrt(nm.var(:))));
 fprintf('Fraction >1 std from avg: %.3f\n',mean(nstd(:)>1));
 fprintf('Fraction >2 std from avg: %.3f\n',mean(nstd(:)>2));
 
 subplot(nplot,1,pnum); pnum=pnum+1;
-imshow(nstd/max(nstd(:)));
-title(sprintf('Unfiltered deviation / %.2f',max(nstd(:))));
+ufscale=10;
+imshow(nstd/ufscale);
+title(sprintf('Unfiltered deviation / %.1f',ufscale));
+
+thresh=p.analysisparams.fgthresh(2);
+
+%subplot(nplot,1,pnum); pnum=pnum+1;
+%hist(min(thresh,nstd(:)),1000);
+%title('Distribution of nstd');
+
+fprintf('Using window size of [%d,%d]\n',args.wsize);
+f=sqrt(mean(imfilter(nstd,fspecial('average',args.wsize)),3));
+
 
 subplot(nplot,1,pnum); pnum=pnum+1;
-hist(nstd(:),1000);
-title('Distribution of nstd');
+imshow(f/thresh);
+title(sprintf('Filtered deviation / %.2f',thresh));
 
-%f=imfilter(nstd,fspecial('average',[5 7]));
-f=imfilter(nstd,fspecial('average',[12 5]));
-fs=f;
-for i=1:3
-  fs(:,:,i)=f(:,:,i)/max(max(f(:,:,i)));
+for i=1:2
+  thresh=p.analysisparams.fgthresh(i);
+  det=f>thresh;
+  rgb=img;
+  rgb(:,:,2)=max(rgb(:,:,2),det);
+  subplot(nplot,1,pnum); pnum=pnum+1;
+  imshow(rgb);
+  title(sprintf('Deviation>%.2f',thresh));
 end
-
-subplot(nplot,1,pnum); pnum=pnum+1;
-imshow(f/max(f(:)));
-title(sprintf('Filtered deviation / %.2f',max(f(:))));
-
-thresh=1.5;
-%thresh=2.25;
-det=f>thresh;
-rgb=img;
-rgb=max(rgb,det);
-subplot(nplot,1,pnum); pnum=pnum+1;
-imshow(rgb);
-title(sprintf('Deviation>%.2f',thresh));
-
-bwdet=det(:,:,1)|det(:,:,2)|det(:,:,3);
-subplot(nplot,1,pnum); pnum=pnum+1;
-imshow(bwdet);
-title('Binary detection');
 
 %subplot(nplot,1,pnum); pnum=pnum+1;
 %hist(f(:),1000);
 %title('Distribution of f');
 
 % Overall plot with LED decisions overlaid
-setfig('noisedet2');clf;
-subplot(211);
+subplot(nplot,1,pnum); pnum=pnum+1;
 %imshow(nstd/max(nstd(:)));
-imshow(f/max(f(:)));
+imshow(f/thresh);
 hold on;
 pc=p.camera(cam).pixcalib;
+vc=p.camera(cam).viscache;
 roi=p.camera(cam).roi([1,3]);
-for l=1:size(vis.corr,2)
-  pl=pc(l).pos-roi;
-  if vis.corr(cam,l)>0.5
+nled=size(vis.corr,2);
+for l=1:nled
+  pl=round((vc.tlpos(l,:)+vc.brpos(l,:))/2);
+  nstd=(1-vis.corr(cam,l))*p.analysisparams.fgscale;
+  if nstd<p.analysisparams.fgthresh(1)
     plot(pl(1),pl(2),'g');
-  elseif ~isnan(vis.corr(cam,l))
+  elseif isnan(nstd) || nstd<p.analysisparams.fgthresh(2)
+    plot(pl(1),pl(2),'y');
+  else
     plot(pl(1),pl(2),'r');
   end
   if ~any(isnan(pl))
-    pf(l,:)=f(pl(2),pl(1),:);
+    pf(l)=f(pl(2),pl(1));
   else
-    pf(l,1:3)=nan;
+    pf(l)=nan;
   end
 end
-subplot(212);
-plot(vis.corr(cam,:),'m');
+subplot(nplot,1,pnum); pnum=pnum+1;
+fecorr=vis.corr(cam,:);
+recorr=1-pf/p.analysisparams.fgscale;
+plot(fecorr,'m');
 hold on;
-plot(1-pf/3);
+plot(recorr);
+plot([1,nled],[vis.mincorr,vis.mincorr],':');
+legend('From FrontEnd','Recomputed');
+title(sprintf('FrontEnd corr (mean=%.3f) and reccomputed (%.3f)', nanmean(fecorr), nanmean(recorr)));
 
-
+subplot(nplot,1,pnum); pnum=pnum+1;
+festd=(1-vis.corr(cam,:))*p.analysisparams.fgscale;
+plot(festd,'m');
+hold on;
+plot(pf);
+plot([1,nled],p.analysisparams.fgthresh(1)*[1,1],':');
+plot([1,nled],p.analysisparams.fgthresh(2)*[1,1],':');
+legend('From FrontEnd','Recomputed');
+title(sprintf('FrontEnd nstd (mean=%.3f) and reccomputed (%.3f)', nanmean(festd), nanmean(pf)));
--- Experimental/noisemodel.m
+++ Experimental/noisemodel.m
@@ -1,10 +1,10 @@
 % Calculate background noise model using vis data
-function nm=noisemodel(vis,cam)
+function nm=noisemodel(p,vis,cam)
 nm.avg=vis.refim{cam};
-nm.std=sqrt(vis.refim2{cam}-vis.refim{cam}.^2);
-% Make sure the std isn't too small -- use 1/3 of mean as clip
-cliplev=mean(nm.std(:))/3;
+nm.var=max(0,vis.refim2{cam}-vis.refim{cam}.^2);
+% Make sure the std isn't too small
+cliplev=p.analysisparams.fgminvar;
 fprintf('Clipping at %.3f\n', cliplev);
-nm.std(nm.std>=0 & nm.std<cliplev)=cliplev;
-nm.std(nm.std<0 & nm.std>-cliplev)=-cliplev;
+nm.var(nm.var>=0 & nm.var<cliplev)=cliplev;
+nm.var(nm.var<0 & nm.var>-cliplev)=-cliplev;
 
