8d6f23265064ffce4eb7f38f23ff72459f2dda28
Brent Townshend <bst@tc.com>
2013-12-07 18:04:23 -0800

    More user-friendly display of clipping

--- Experimental/noisemodel.m
+++ Experimental/noisemodel.m
@@ -4,7 +4,7 @@ nm.avg=vis.refim{cam};
 nm.var=vis.refim2{cam};
 % Make sure the std isn't too small
 cliplev=p.analysisparams.fgminvar;
-fprintf('Clipping at %.3f\n', cliplev);
+fprintf('Using minimum value of (%.1f/255)^2 for variances\n', 255*sqrt(cliplev));
 nm.var(nm.var>=0 & nm.var<cliplev)=cliplev;
 nm.var(nm.var<0 & nm.var>-cliplev)=-cliplev;
 
