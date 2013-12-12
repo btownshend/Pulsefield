22913e14f8f98351cd9559099f2d47ea264c0ebc
Brent Townshend <bst@tc.com>
2013-12-11 22:06:36 -0800

    Many changes to VisualizerChuck

--- Video/Tracker/src/VisualizerChuck.java
+++ Video/Tracker/src/VisualizerChuck.java
@@ -5,6 +5,7 @@ import processing.core.PConstants;
 import processing.core.PVector;
 import processing.opengl.PGL;
 import processing.opengl.PGraphicsOpenGL;
+import java.util.ArrayList;
 
 // Visualizer that sends messages to chuck
 abstract class Fiducial extends Position {
@@ -22,14 +23,29 @@ abstract class Fiducial extends Position {
 
 class CCPair {
 	int cc1,cc2;
+	String nm1,nm2;
+	PVector rng1, rng2;
 	int color;
 	CCPair() {
 		cc1=-1;
 		cc2=-1;
 	}
-	CCPair(int cc1, int cc2, int color) {
+	CCPair(int cc1, String nm1,PVector rng1, int cc2, String nm2,PVector rng2, int color) {
 		this.cc1=cc1;
 		this.cc2=cc2;
+		this.nm1=nm1;
+		this.nm2=nm2;
+		this.color=color;
+		this.rng1=rng1;
+		this.rng2=rng2;
+	}
+	CCPair(int cc1, String nm1, int cc2, String nm2, int color) {
+		this.cc1=cc1;
+		this.cc2=cc2;
+		this.nm1=nm1;
+		this.nm2=nm2;
+		this.rng1=new PVector(0f,128.0f);
+		this.rng2=new PVector(0f,128.0f);
 		this.color=color;
 	}
 }
@@ -45,7 +61,7 @@ class GeneratorType {
 		this.code=code;
 		this.cclist=new CCPair[2];
 		cclist[0]=cc1;
-		cclist[1]=new CCPair(-1,-1,0);
+		cclist[1]=new CCPair(-1,"",-1,"",0);
 	}
 	GeneratorType(String name, int color, int code, CCPair cc1, CCPair cc2) {
 		this.name=name;
@@ -55,20 +71,48 @@ class GeneratorType {
 		cclist[0]=cc1;
 		cclist[1]=cc2;
 	}
+	GeneratorType(String name, int color, int code, CCPair cc1, CCPair cc2, CCPair cc3) {
+		this.name=name;
+		this.color=color;
+		this.code=code;
+		this.cclist=new CCPair[3];
+		cclist[0]=cc1;
+		cclist[1]=cc2;
+		cclist[2]=cc3;
+	}
+	GeneratorType(String name, int color, int code, CCPair cc1, CCPair cc2, CCPair cc3, CCPair cc4) {
+		this.name=name;
+		this.color=color;
+		this.code=code;
+		this.cclist=new CCPair[4];
+		cclist[0]=cc1;
+		cclist[1]=cc2;
+		cclist[2]=cc3;
+		cclist[3]=cc4;
+	}
 }
 
 
 class Generator extends Fiducial {
+	static final CCPair cc1_11=new CCPair(1,"LFODepth",11,"LFOSpd",0xffff00ff);
+	static final CCPair ccpat=new CCPair(200,"Pattern",new PVector(0.0f,1.99f),-1,"-",new PVector(0.0f,0.0f),0xff007f4f);
+	static final CCPair reverb=new CCPair(201,"Reverb",new PVector(0.0f,100f),-1,"-",new PVector(0.0f,0.0f),0xff8000ff);
 	static final GeneratorType genTypes[] = {
-		new GeneratorType("FM",0xff0000ff,0,new CCPair(1,11,0xffff00ff)),
-		new GeneratorType("BeeThree",0xffff0000,1,new CCPair(2,4,0xffffff00),new CCPair(1,11,0xffff00ff)),
-		new GeneratorType("Mandolin",0xff00ff00,2,new CCPair(2,4,0xffffff00),new CCPair(1,11,0xffff00ff))
+		new GeneratorType("FM",0xff0000ff,0,new CCPair(1,"LFODepth",new PVector(0.0f,500.0f),11,"LFOSpd",new PVector(1.0f,500.0f),0xffff00ff),
+					ccpat,reverb),
+		new GeneratorType("BeeThree",0xffff0000,1,new CCPair(2,"Feedback",4,"Op3Gain",0xffffff00),
+					ccpat, cc1_11,reverb),
+		new GeneratorType("ModalBar",0xff00ff00,2,new CCPair(16,"Preset",new PVector(0.0f,8.99f),1,"Mix",new PVector(0.0f,1.0f),0xffffff00),
+					ccpat,new CCPair(2,"Hardness",4,"Position",0xffff00ff),
+					reverb),
+		new GeneratorType("FMVoices",0xff7f7f40,1,new CCPair(2,"Vowel",4,"Tilt",0xff7fff80),
+					ccpat,reverb,cc1_11),
 	};
 
 	static final int NUMGENTYPES=genTypes.length;
 
 	GeneratorType genType;
-	int nControllers;
+	ArrayList<Controller> children;
 
 	Generator(Position ps, GeneratorType genType) {
 		super(ps);
@@ -77,15 +121,17 @@ class Generator extends Fiducial {
 		msg.add(id);
 		msg.add(genType.code);
 		Tracker.sendOSC("CK",msg);
-		nControllers=0;
+		children=new ArrayList<Controller>();
 	}
-	
+
 	@Override() 
 	void stop() {
 		OscMessage msg = new OscMessage("/chuck/del");
 		msg.add(id);
 		Tracker.sendOSC("CK",msg);
 		PApplet.println("Stopped generator ID "+id);
+		while (children.size()>0)
+			children.get(0).disconnect();
 	}
 
 	@Override
@@ -107,10 +153,10 @@ class Generator extends Fiducial {
 		parent.stroke(genType.color);
 		parent.ellipseMode(PConstants.CENTER);
 		parent.ellipse(x, y, sz, sz);
-		parent.fill(255,255);
+		parent.fill(255);
 		parent.textSize(sz*.7f);
-		parent.textAlign(PConstants.CENTER,PConstants.CENTER);
-		parent.text(genType.name,x,y);
+		parent.textAlign(PConstants.CENTER,PConstants.BOTTOM);
+		parent.text(genType.name,x,y-sz/2);
 	}
 }
 
@@ -118,7 +164,7 @@ class Controller extends Fiducial {
 	Generator parent;
 	CCPair cc;   // Controller numbers to use for (dir,dist), -1 to ignore
 	float cc1val, cc2val;
-	
+
 	Controller(Position ps) {
 		super(ps);
 		parent=null;
@@ -136,7 +182,7 @@ class Controller extends Fiducial {
 				msg.add(cc.cc1);
 				cc1val = (float) (dir/(2*Math.PI)-0.25f);  // Map so up on screen is 0.0 and increases CW
 				if (cc1val<0)cc1val=cc1val+1f;
-				cc1val *= 128;
+				cc1val=cc1val*(cc.rng1.y-cc.rng1.x)+cc.rng1.x;
 				msg.add(cc1val);
 				Tracker.sendOSC("CK",msg);
 			}
@@ -144,9 +190,10 @@ class Controller extends Fiducial {
 				OscMessage msg = new OscMessage("/chuck/dev/"+parent.id+"/cc");
 				msg.add(cc.cc2);
 				// Linear from 0.0 at DISTBREAK to 1.0 at distance DISTCREATE
-				cc2val=128*(1.0f-dist/(Fiducials.DISTBREAK-Fiducials.DISTCREATE));
-				if (cc2val>128.0f) cc2val=128.0f;
+				cc2val=(1.0f-dist/(Fiducials.DISTBREAK-Fiducials.DISTCREATE));
+				if (cc2val>1.0f) cc2val=1.0f;
 				if (cc2val<0.0f) cc2val=0.0f;
+				cc2val=cc2val*(cc.rng2.y-cc.rng2.x)+cc.rng2.x;
 				msg.add(cc2val);  
 				Tracker.sendOSC("CK",msg);
 			}
@@ -161,38 +208,65 @@ class Controller extends Fiducial {
 			parent.fill(cc.color);
 			parent.stroke(cc.color);
 		}
-		parent.rectMode(PConstants.CENTER);
-		parent.rect(x, y, sz, sz);
-		// parent.triangle(x-sz/2,y+sz/2,x+sz/2,y+sz/2,x,y-sz/2);
 		if (this.parent!=null) {
 			parent.line((origin.x+1)*wsize.x/2, (origin.y+1)*wsize.y/2, (this.parent.origin.x+1)*wsize.x/2, (this.parent.origin.y+1)*wsize.y/2);
 		}
-		parent.fill(255,255);
+		if (cc != null && cc.cc1==200)
+			// Use triangles for patterns
+			parent.triangle(x-sz/2,y+sz/2,x+sz/2,y+sz/2,x,y-sz/2);
+		else {
+			parent.rectMode(PConstants.CENTER);
+			parent.rect(x, y, sz, sz);
+		}
+		parent.fill(255);
 		parent.textSize(sz*.5f);
-		parent.textAlign(PConstants.CENTER,PConstants.CENTER);
 		if (cc!=null) {
-			if (cc.cc1!=-1)
-				parent.text(String.format("CC%d=%.0f",cc.cc1,cc1val),x,y-sz*0.3f);
-			if (cc.cc2!=-1)
-				parent.text(String.format("CC%d=%.0f",cc.cc2,cc2val),x,y+sz*0.3f);
+			if (cc.cc1!=-1) {
+				parent.textAlign(PConstants.CENTER,PConstants.BOTTOM);
+				parent.text(String.format("%s=%d",cc.nm1,(int)cc1val),x,y-sz/2);
+			}
+			if (cc.cc2!=-1) {
+				parent.textAlign(PConstants.CENTER,PConstants.TOP);
+				parent.text(String.format("%s=%d",cc.nm2,(int)cc2val),x,y+sz/2);
+			}
 		}
 	}
 
 	void connect(Generator parent) {
-		if (parent.nControllers >= parent.genType.cclist.length) {
-			PApplet.println("Not connecting additional controller to "+parent.id+", which already has "+parent.nControllers+" controllers");
+		if (parent.children.size() >= parent.genType.cclist.length) {
+			PApplet.println("Not connecting additional controller to "+parent.id+", which already has "+parent.children.size()+" controllers");
 			return;
 		}
 		this.parent=parent;
-		this.cc=parent.genType.cclist[parent.nControllers];
+		// Find a controller not being used
+		int newcc;
+		while (true) {
+			newcc = (new java.util.Random()).nextInt(parent.genType.cclist.length);
+			PApplet.println("Trying newcc="+newcc+": "+parent.genType.cclist[newcc].cc1+","+parent.genType.cclist[newcc].cc2);
+			boolean inuse=false;
+			for (int i=0;i<parent.children.size();i++) {
+				PApplet.println("parent.children["+i+"] = "+parent.children.get(i).cc.cc1+","+parent.children.get(i).cc.cc2);
+				if (parent.children.get(i).cc == parent.genType.cclist[newcc]) {
+					PApplet.println("inuse");
+					inuse=true;
+					break;
+				}
+			}
+			PApplet.println("newcc="+newcc+" inuse:"+inuse);
+			if (!inuse)
+				break;
+		}
+		this.cc=parent.genType.cclist[newcc];
+		parent.children.add(this);
 		PApplet.println("Connect controller "+id+" to generator "+parent.id+" on controllers "+this.cc.cc1+","+this.cc.cc2);
-		parent.nControllers++;
 	}
 
 	void disconnect() {
 		PApplet.println("Disconnect controller "+id+" from generator "+parent.id);
-		parent.nControllers--;
+		// Remove from parent list
+		parent.children.remove(this);
 		parent=null;
+		cc=null;
 	}
 }
 
@@ -206,11 +280,13 @@ class Fiducials extends HashMap<Integer,Fiducial> {
 	}
 
 	void removeAll() {
-		for (Fiducial f: values())
+		for (Fiducial f: values()) {
+			PApplet.println("removeall: removing id "+f.id);
 			f.stop();
+		}
 		super.clear();
 	}
-	
+
 	void addNew(Position pos, int id) {
 		int ngen=0, nctrl=0;
 		for (Fiducial f: values()) {
@@ -219,12 +295,13 @@ class Fiducials extends HashMap<Integer,Fiducial> {
 			else
 				nctrl++;
 		}
-		if (ngen==0 || nctrl>ngen)
-			put(id, new Generator(pos,Generator.genTypes[ngen%Generator.NUMGENTYPES]));
-		else
+		if (ngen==0 || (nctrl>0 && (new java.util.Random()).nextFloat() > 1.0f*ngen/(ngen+nctrl))) {
+			int genType=(new java.util.Random()).nextInt(Generator.NUMGENTYPES);
+			put(id, new Generator(pos,Generator.genTypes[genType]));
+		} else
 			put(id, new Controller(pos));
 	}
-	
+
 	void remove(int id) {
 		for (Fiducial f: values()) {
 			if (f.id==id) {
@@ -275,12 +352,24 @@ class Fiducials extends HashMap<Integer,Fiducial> {
 
 	void draw(PApplet parent, PVector wsize) {
 		float sz=30;
+		// Draw all the controllers first so generators will be on top of connecting lines
 		for (Fiducial f: values()) {
-			int c=f.getcolor(parent);
-			parent.fill(c,255);
-			parent.stroke(c,255);
-			parent.strokeWeight(5);
-			f.draw(parent,wsize,sz);
+			if (f instanceof Controller) {
+				int c=f.getcolor(parent);
+				parent.fill(c,255);
+				parent.stroke(c,255);
+				parent.strokeWeight(5);
+				f.draw(parent,wsize,sz);
+			}
+		}
+		for (Fiducial f: values()) {
+			if (f instanceof Generator) {
+				int c=f.getcolor(parent);
+				parent.fill(c,255);
+				parent.stroke(c,255);
+				parent.strokeWeight(5);
+				f.draw(parent,wsize,sz);
+			}
 		}
 	}
 }
@@ -336,7 +425,7 @@ public class VisualizerChuck extends Visualizer {
 	@Override
 	public void draw(PApplet parent, Positions p, PVector wsize) {
 		PGL pgl=PGraphicsOpenGL.pgl;
-		pgl.blendFunc(PGL.SRC_ALPHA, PGL.DST_ALPHA);
+		pgl.blendFunc(PGL.SRC_ALPHA, PGL.DST_ALPHA);  // Don't blend opaque colors
 		pgl.blendEquation(PGL.FUNC_ADD);  
 		parent.background(0, 0, 0);  
 		parent.colorMode(PConstants.RGB, 255);
@@ -351,5 +440,6 @@ public class VisualizerChuck extends Visualizer {
 		fiducials.draw(parent,wsize);
 	}
 }
+
 
 
