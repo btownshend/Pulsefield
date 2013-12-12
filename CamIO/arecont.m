e04eaf8414baae070e9ed1010c698b931accbd99
Brent Townshend <bst@tc.com>
2013-12-12 11:58:18 -0800

    Added option for half resolution

--- CamIO/arecont.m
+++ CamIO/arecont.m
@@ -1,6 +1,14 @@
-function p=arecont(id)
+function p=arecont(id,half)
+if nargin<2
+  half=0;
+end
 [h,port]=getsubsysaddr(sprintf('CA%d',id),'reload',false);
-url=sprintf('http://%s/image?res=full&quality=21&doublescan=0&x0=0&x1=9999&y0=0&y1=9999',h);
+if half
+  res='half';
+else
+  res='full';
+end
+url=sprintf('http://%s/image?res=%s&quality=21&doublescan=0&x0=0&x1=9999&y0=0&y1=9999',h,res);
 cmd=sprintf('DYLD_LIBRARY_PATH=/opt/local/lib;curl -s ''%s'' >/tmp/im.jpg', url)
 p.captstart=now;
 [s,r]=system(cmd);
