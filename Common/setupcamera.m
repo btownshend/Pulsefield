b44fc59bc9f14933f141258d75c7e1fc87bae1c4
Brent Townshend <bst@tc.com>
2014-03-04 23:45:18 -0800

    Added av10115-4mm to setupcamera.m

--- Common/setupcamera.m
+++ Common/setupcamera.m
@@ -44,6 +44,13 @@ elseif strcmp(cam.type,'av10115')
   cam.sensorwidth=1.67*cam.hpixels/1000; % Sensor width (mm)
   cam.fisheye=1;
   cam.fov=145*pi/180;
+elseif strcmp(cam.type,'av10115-4mm')
+  % Arecont Vision AV10115 with Arecont 4mm 
+  cam.hpixels=3648;
+  cam.vpixels=2752;
+  cam.sensorwidth=1.67*cam.hpixels/1000; % Sensor width (mm)
+  cam.fisheye=0;
+  cam.lens=4.0;
 elseif strcmp(cam.type,'av10115-half')
   % Arecont Vision AV10115 with SUNEX 2.67mm fisheye (half resolution mode)
   cam.hpixels=3648/2;
