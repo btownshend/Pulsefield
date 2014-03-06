d3c542bb34d77fc7e672491f7ef871cf500f4476
Brent Townshend <bst@tc.com>
2014-03-05 22:56:50 -0800

    Have separate counts for updating background depending on whether its closer or farther

--- LIDAR/Matlab/updatebg.m
+++ LIDAR/Matlab/updatebg.m
@@ -1,20 +1,11 @@
 % Update background model
 function bg=updatebg(bg,vis,varargin)
-defaults=struct('minsep',0.3,'tc',100,'mincnt',40,'debug',false);
+defaults=struct('minsep',0.1,'tc',100,'mincntcloser',800,'mincntfarther',40,'debug',false);
 args=processargs(defaults,varargin);
 
 if isempty(bg) 
-  bg=struct('range',vis.range,'newrange',vis.range,'newcnt',zeros(1,length(vis.range)),'angle',vis.angle);
+  bg=struct('range',vis.range,'newrange',vis.range,'newcnt',zeros(length(vis.range),1),'angle',vis.angle);
   return;
-end
-
-farther=vis.range>bg.range+args.minsep;
-if sum(farther)>0
-  if args.debug
-    fprintf('Moving %d background pixels to a more distant target\n', sum(farther));
-  end
-  bg.range(farther)=vis.range(farther);
-  bg.newcnt(farther)=0;
 end
 
 isbg=false(1,length(bg.range));
@@ -29,10 +20,12 @@ bg.range(isbg)=vis.range(isbg)/args.tc+bg.range(isbg)*(1-1/args.tc);
 bg.newcnt(~isnewbg)=0;   % Reset count if we're not close to new background
 bg.newrange(bg.newcnt==0)=vis.range(bg.newcnt==0);   % Reset to current position
 bg.newcnt(isnewbg)=bg.newcnt(isnewbg)+1;  % Increase count if we are
-update=bg.newcnt>args.mincnt;
+update=((bg.newcnt>args.mincntcloser & bg.newrange(:)<bg.range(:)) | (bg.newcnt>args.mincntfarther & bg.newrange(:)>bg.range(:)));
 if sum(update)>0
   if args.debug
-    fprintf('Updating %d background pixels\n', sum(update));
+    ncloser=sum(update(bg.newrange(:)<bg.range(:)));
+    nfarther=sum(update(bg.newrange(:)>bg.range(:)));
+    fprintf('** Updating %d closer, %d farther background pixels\n', ncloser, nfarther);
   end
   bg.range(update)=bg.newrange(update);
   bg.newcnt(update)=0;
