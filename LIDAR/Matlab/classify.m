f78a6ad27a820c5dd2b9527f696cacc27f774462
Brent Townshend <bst@tc.com>
2014-03-05 22:54:56 -0800

    Corrected computation of bounding box

--- LIDAR/Matlab/classify.m
+++ LIDAR/Matlab/classify.m
@@ -24,7 +24,6 @@ for i=1:max(class)
   sel=class==i;
   xy=range2xy(vis.angle(sel)+pi/2,vis.range(sel));
   pos(i,:)=mean(xy,1);
-  radius=0.15;
-  bbox(i,:)=[pos(i,1)-radius,pos(i,2)-radius,2*radius,2*radius];
+  bbox(i,:)=[min(xy(:,1)),min(xy(:,2)),max(xy(:,1))-min(xy(:,1)),max(xy(:,2))-min(xy(:,2))];
 end
 targets=struct('class',class,'pos',pos,'bbox',bbox);
