a13c319773a542afbf6925b356a0bd11a95beb43
Brent Townshend <bst@tc.com>
2014-03-08 16:27:16 -0500

    Keep track of whether targets are bounded

--- LIDAR/Matlab/calcbboxes.m
+++ LIDAR/Matlab/calcbboxes.m
@@ -6,6 +6,7 @@ MAXSPECIAL=2;
 % Calculate centroids and bounding boxes, pack class numbers
 pos=[];
 bbox=[];
+bounded=[];
 for i=MAXSPECIAL+1:max(vis.class)
   sel=vis.class==i;
   if sum(sel)==0
@@ -13,7 +14,9 @@ for i=MAXSPECIAL+1:max(vis.class)
   end
   pos(end+1,:)=mean(vis.xy(sel,:),1);
   bbox(end+1,:)=[min(vis.xy(sel,1)),min(vis.xy(sel,2)),max(vis.xy(sel,1))-min(vis.xy(sel,1)),max(vis.xy(sel,2))-min(vis.xy(sel,2))];
+  fsel=find(sel);
+  bounded(end+1)=~vis.shadowed(fsel(1)) && ~vis.shadowed(fsel(end));
   cnum=size(pos,1)+MAXSPECIAL;
   vis.class(vis.class==i)=cnum;
 end
-vis.targets=struct('pos',pos,'bbox',bbox);
+vis.targets=struct('pos',pos,'bbox',bbox,'bounded',bounded);
