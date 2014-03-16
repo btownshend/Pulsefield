9f474155440f9c70eacf748abddd9fcf64564c4c
Brent Townshend <bst@tc.com>
2014-03-15 21:42:51 -0700

    Modified shadow calculations slightly

--- LIDAR/Matlab/moveintoshadows.m
+++ LIDAR/Matlab/moveintoshadows.m
@@ -13,26 +13,27 @@ for i=1:length(tracker.tracks)
         theta=vis.angle(end);
       end
       newpos=range2xy(theta,range);
-      closest=norm(newpos-t.updatedLoc)^2;
     else
       [~,cpos]=min(abs(vis.angle-theta));
-      if vis.range(cpos)<range
+      % Actually, don't make background special, could have a near background (like a chair) that targets go behind
+      if vis.range(cpos)<range % && bg.range(cpos)>range
         % Already shadowed
         continue;
       else
         % Current prediction is not shadowed
         fprintf('Track %d at (%.2f,%.2f) is invisible, but not shadowed\n',t.id, t.updatedLoc);
         rangetmp=max(vis.range+0.2,range);   % Nearest posible locations of each shadow
-        rangetmp(vis.class==0)=vis.range(vis.class==0); % Don't make background a shadow possibility, but could stick to background
+        %rangetmp(vis.class==0)=vis.range(vis.class==0); % Don't make background a shadow possibility, but could stick to background
         closexy=range2xy(vis.angle,rangetmp);
         distxy=closexy;
         distxy(:,1)=distxy(:,1)-t.updatedLoc(1);
         distxy(:,2)=distxy(:,2)-t.updatedLoc(2);
-        [closest,cpos]=min(distxy(:,1).^2+distxy(:,2).^2);
+        [~,cpos]=min(distxy(:,1).^2+distxy(:,2).^2);
         newpos=closexy(cpos,:);
       end
     end
-    fprintf('Moving to closest valid point at (%.2f,%.2f) with a distance of %.2f\n', newpos, sqrt(closest));
+    distmoved=norm(newpos-t.updatedLoc);
+    fprintf('Moving to closest valid point at (%.2f,%.2f) with a distance of %.2f\n', newpos, distmoved);
     t.updatedLoc=newpos;
     newstate=t.kalmanFilter.State;
     newstate([1,3])=newpos;
