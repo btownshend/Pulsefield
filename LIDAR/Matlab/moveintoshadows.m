3717531639fb07a2554e586ccfdd8c7ede8abe36
Brent Townshend <bst@tc.com>
2014-03-16 00:30:17 -0700

    Update predictedLoc, improved debug

--- LIDAR/Matlab/moveintoshadows.m
+++ LIDAR/Matlab/moveintoshadows.m
@@ -4,9 +4,9 @@ for i=1:length(tracker.tracks)
   t=tracker.tracks(i);
   if t.consecutiveInvisibleCount>0
     % Not visible
-    [theta,range]=xy2range(t.updatedLoc);
+    [theta,range]=xy2range(t.predictedLoc);
     if theta<vis.angle(1) || theta>vis.angle(end)
-      fprintf('Track %d at (%.2f,%.2f) is outside FOV\n',t.id, t.updatedLoc);
+      fprintf('Frame %d, track %d at (%.2f,%.2f) is outside FOV\n',vis.frame,t.id, t.predictedLoc);
       if theta<vis.angle(1)
         theta=vis.angle(1);
       else
@@ -21,25 +21,28 @@ for i=1:length(tracker.tracks)
         continue;
       else
         % Current prediction is not shadowed
-        fprintf('Track %d at (%.2f,%.2f) is invisible, but not shadowed\n',t.id, t.updatedLoc);
+        fprintf('Frame %d, track %d at (%.2f,%.2f) is invisible, but not shadowed\n',vis.frame,t.id, t.predictedLoc);
         rangetmp=max(vis.range+0.2,range);   % Nearest posible locations of each shadow
         %rangetmp(vis.class==0)=vis.range(vis.class==0); % Don't make background a shadow possibility, but could stick to background
         closexy=range2xy(vis.angle,rangetmp);
         distxy=closexy;
-        distxy(:,1)=distxy(:,1)-t.updatedLoc(1);
-        distxy(:,2)=distxy(:,2)-t.updatedLoc(2);
+        distxy(:,1)=distxy(:,1)-t.predictedLoc(1);
+        distxy(:,2)=distxy(:,2)-t.predictedLoc(2);
         [~,cpos]=min(distxy(:,1).^2+distxy(:,2).^2);
         newpos=closexy(cpos,:);
       end
     end
-    distmoved=norm(newpos-t.updatedLoc);
+    distmoved=norm(newpos-t.predictedLoc);
     fprintf('Moving to closest valid point at (%.2f,%.2f) with a distance of %.2f\n', newpos, distmoved);
-    t.updatedLoc=newpos;
+    t.predictedLoc=newpos;
     newstate=t.kalmanFilter.State;
     newstate([1,3])=newpos;
-    newstate([2,4])=newstate([2,4])/2;   % Also decrease velocity
+    %newstate([2,4])=newstate([2,4])/2;   % Also decrease velocity
     t.kalmanFilter.State=newstate;
     tracker.tracks(i)=t;
+    if distmoved>1
+      keyboard
+    end
   end
 end
 
