e10274ec1ca5fba16a70dc9405dc69a79ce7e731
Brent Townshend <bst@tc.com>
2014-03-24 07:46:43 +0700

    Added recompute()

---
+++ LIDAR/Matlab/recompute.m
@@ -0,0 +1,26 @@
+% Recompute given frame from snap
+function recompute(snap,frame)
+params=getparams();
+
+frames=arrayfun(@(z) z.vis.frame, snap);
+index=find(frame==frames);
+if isempty(index)
+  fprintf('Frame not found\n');
+  return;
+end
+vis=snap(index).vis;
+if index==1
+  npredict=1
+  tracker=World();
+  bg=Background(vis);
+else
+  npredict=frame-snap(index-1).vis.frame;
+  tracker=snap(index-1).tracker.clone();
+  tracker.debug=true;
+  bg=snap(index-1).bg.clone();
+  bg.update(vis);
+end
+fprintf('Recomputing snap(%d) - frame %d with %d frame steps\n', index,frame,npredict);
+vis=classify(vis,bg,'debug',true);
+vis=splitclasses(vis);
+tracker.update(vis,npredict,params.fps);
