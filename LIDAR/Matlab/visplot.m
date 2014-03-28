15fa9eb0d8fa26ac770c602149d184629e0a4800
Brent Townshend <bst@tc.com>
2014-03-28 16:16:22 +0700

    Removed use of obsolete vis.targets

--- LIDAR/Matlab/visplot.m
+++ LIDAR/Matlab/visplot.m
@@ -2,20 +2,13 @@ function visplot(vis)
 xy=range2xy(vis.angle,vis.range);
 setfig('visplot');clf;
 hold on;
-if isfield(vis,'targets')
-  col=vis.class+1;
-else
-  col=ones(size(xy,1),1);
-end
+col=ones(size(xy,1),1);
 colors='rgbcmk';
 col(col>length(colors))=length(colors);
 for i=1:size(xy,1)
   plot(xy(i,1),xy(i,2),['.',colors(col(i))]);
 end
 hold on;
-if isfield(vis,'targets') && size(vis.targets.pos,1)>0
-  plot(vis.targets.pos(:,1),vis.targets.pos(:,2),'o');
-end
 if isfield(vis,'truth')
   t=vis.truth;
   plot(0,0,'o');
