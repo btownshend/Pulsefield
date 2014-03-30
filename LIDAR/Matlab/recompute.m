c2a8dcfec893082ec135e97b97780c2d9a45c3fd
Brent Townshend <bst@tc.com>
2014-03-30 06:37:23 -0700

    Reorganized recompute() so that it would not access tracker until needed

--- LIDAR/Matlab/recompute.m
+++ LIDAR/Matlab/recompute.m
@@ -11,20 +11,26 @@ end
 vis=snap(index).vis;
 if index==1
   npredict=1
-  tracker=World();
   bg=Background(vis);
 else
   npredict=frame-snap(index-1).vis.frame;
-  tracker=snap(index-1).tracker.clone();
   bg=snap(index-1).bg.clone();
   bg.update(vis);
+end
+fprintf('Recomputing snap(%d) - frame %d with %d frame steps\n', index,frame,npredict);
+vis=classify(vis,bg,'debug',true);
+vis=splitclasses(vis,'debug',true);
+first=find(vis.class>1,1);
+last=find(vis.class>1,1,'last');
+fprintf('classes(%d:%d)=%s\n', first,last,sprintf('%d ',vis.class(first:last)));
+if index==1
+  tracker=World();
+else
+  tracker=snap(index-1).tracker.clone();
 end
 tracker.debug=true;
 for i=1:length(tracker.tracks)
   tracker.tracks(i).debug=true;
 end
-fprintf('Recomputing snap(%d) - frame %d with %d frame steps\n', index,frame,npredict);
-vis=classify(vis,bg,'debug',true);
-vis=splitclasses(vis,'debug',true);
 tracker.update(vis,npredict,params.fps);
 plotsnap(struct('vis',vis,'tracker',tracker,'bg',snap(index).bg));
