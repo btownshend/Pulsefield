15f93d5fc95229fddc34bc9ade04f392034e2ec2
Brent Townshend <bst@tc.com>
2014-03-30 06:38:10 -0700

    Update comparesnaps() to compare shadowed[]

--- LIDAR/Matlab/comparesnaps.m
+++ LIDAR/Matlab/comparesnaps.m
@@ -7,14 +7,23 @@ for i=1:length(frame)
   c1=s1(f1==f).vis.class;
   c2=s2(f2==f).vis.class;
   if any(c1~=c2)
-    fprintf('Frame %d (%d,%d) has %d differences: ', f, find(f1==f), find(f2==f), sum(c1~=c2));
+    fprintf('Frame %d (%d,%d) has %d class differences: ', f, find(f1==f), find(f2==f), sum(c1~=c2));
     for i=1:length(c1)
       if c1(i)~=c2(i)
         fprintf('%d:%d,%d ', i, c1(i), c2(i));
       end
     end
     fprintf('\n');
-  else
-    fprintf('Frame %d (%d,%d) identical\n', f, find(f1==f), find(f2==f));
+  end
+  b1=s1(f1==f).vis.shadowed;
+  b2=s2(f2==f).vis.shadowed;
+  if any(b1(:)~=b2(:))
+    fprintf('Frame %d (%d,%d) has %d shadowing differences: ', f, find(f1==f), find(f2==f), sum(b1(:)~=b2(:)));
+    for i=1:size(b1,1)
+      if b1(i,1)~=b2(i,1) || b1(i,2)~=b2(i,2)
+        fprintf('%d:[%d,%d],[%d,%d] ', i, b1(i,:), b2(i,:));
+      end
+    end
+    fprintf('\n');
   end
 end
