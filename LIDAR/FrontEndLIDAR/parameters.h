d7e64e4658ca27521c9c23b5f164983651231b7b
Brent Townshend <bst@tc.com>
2014-04-12 17:49:44 -0700

    Reorganized parameters, added some new ones

--- LIDAR/FrontEndLIDAR/parameters.h
+++ LIDAR/FrontEndLIDAR/parameters.h
@@ -31,7 +31,6 @@ static const float INTERPSCANBGWEIGHT=0.2;	// scaling of background probability 
 static const float MINFORCELIKE=-10;  // Minimum likelihood to force assigning a class to the only target that is possible (otherwise a track is formed)
 
 // ***** Tracking
-static const float INITLEGDIAM=200;	// Initial diameter of legs
 static const float INITIALPOSITIONVAR=100*100;  // Variance of initial position estimates
 static const float MAXPOSITIONVAR=1000*1000;  // Never let the position variance go above this during predictions
 static const float INITIALHIDDENVAR=300*300;  // Variance of initial position estimates when leg is not initially visible
@@ -42,10 +41,13 @@ static const float VELDAMPING=0.95;	// Damping (multiplicative factor) for legve
 static const float LEGDIAMTC=100; 	// Time constant for updating diameter estimate of leg
 static const float DRIFTVAR=100*100;	// Additional variance of position estimates per step when they are estimated 
 static const float LEFTNESSTC=500;	// time constant for updating leftness
+static const float MINLIKEFORUPDATES=-30;	  // Minimum likelihood of a target to use the current observations to update it
+
+// ******** Leg statistics 
+static const float INITLEGDIAM=200;	// Initial diameter of legs
+static const float LEGDIAMSTD=50;		// Sigma for leg diameter
 static const float MEANLEGSEP=300; 	// Mean leg separation
 static const float LEGSEPSTD=100;		// Sigma for leg separation
-static const float LEGDIAMSTD=50;		// Sigma for leg diameter
-static const float MINLIKEFORUPDATES=-30;	  // Minimum likelihood of a target to use the current observations to update it
 
 // ******** Deleting tracks
 static const int INVISIBLEFORTOOLONG=50;	// Number of frames of invisible before deleting
@@ -56,4 +58,10 @@ static const float MINVISIBILITY=0.9;	// Minimum visibility to maintain new trac
 static const float ENTRYRATE=1.0; 	// Entries per minute
 static const int MINCREATEHITS=5;	// Need this number of scan targets to create a new track
 
+// ******** Grouping
+static const float GROUPDIST=500;		// Distance to form a group
+static const float UNGROUPDIST=1000;	// Distance to break a group
+
+// ******** Channels
+static const int NCHANNELS=16;
 #endif  /* PARAMETERS_H_ */
