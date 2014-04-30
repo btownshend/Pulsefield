f66b55f14ae0ae99edc108388a07a32632cc4b9d
Brent Townshend <bst@tc.com>
2014-04-29 23:42:36 -0700

    Import ofxEtherdream.* from ofxEtherdream add-on

---
+++ Laser/ofxEtherdream.h
@@ -0,0 +1,74 @@
+//
+//  ofxEtherdream.h
+//  ofxILDA
+//
+//  Created by Daito Manabe + Yusuke Tomoto (Rhizomatiks)
+//  Mods by Memo Akten
+//
+//
+
+
+#include "ofMain.h"
+#include "etherdream.h"
+#include "ofxIldaFrame.h"
+
+class ofxEtherdream : public ofThread {
+public:
+    ofxEtherdream():state(ETHERDREAM_NOTFOUND), bAutoConnect(false) {}
+    
+    ~ofxEtherdream() {
+        kill();
+    }
+    
+    bool stateIsFound();
+    
+    void kill() {
+        clear();
+        stop();
+        if(stateIsFound()) {
+            etherdream_stop(device);
+            etherdream_disconnect(device);
+        }
+    }
+    
+    void setup(bool bStartThread = true);
+    virtual void threadedFunction();
+    
+    
+    // check if the device has shutdown (weird bug in etherdream driver) and reconnect if nessecary
+    bool checkConnection(bool bForceReconnect = true);
+    
+    void clear();
+    void start();
+    void stop();
+
+    void addPoints(const vector<ofxIlda::Point>& _points);
+    void addPoints(const ofxIlda::Frame &ildaFrame);
+    
+    void setPoints(const vector<ofxIlda::Point>& _points);
+    void setPoints(const ofxIlda::Frame &ildaFrame);
+    
+    void send();
+    
+    void setPPS(int i);
+    int getPPS() const;
+    
+    void setWaitBeforeSend(bool b);
+    bool getWaitBeforeSend() const;
+    
+private:
+    void init();
+    
+private:
+    enum {
+        ETHERDREAM_NOTFOUND = 0,
+        ETHERDREAM_FOUND
+    } state;
+    
+    int pps;
+    bool bWaitBeforeSend;
+    bool bAutoConnect;
+    
+    struct etherdream *device;
+    vector<ofxIlda::Point> points;
+};
