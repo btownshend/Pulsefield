a7ed112f491020a453be0d61c93c051798b0635a
Brent Townshend <bst@tc.com>
2014-05-14 13:10:08 -0700

    Updated drawspot.sh

--- Laser/drawspot.sh
+++ Laser/drawspot.sh
@@ -1,6 +1,11 @@
 # Draw a pattern
 replayOSC -h 127.0.0.1 7780 <<EOF
-/laser/circle 0.0 0.0 500.0 0.0 1.0 1.0
-/laser/circle 2000.0 0.0 500.0 0.0 1.0 1.0
-/laser/update 500
+/pf/frame 101
+/pf/set/minx -3.0
+/pf/set/maxx 3.0
+/pf/set/miny 0.0
+/pf/set/maxy 3.0
+/laser/set/color 1.0 1.0 1.0
+/laser/circle 0.0 0.0 1.0
+/laser/circle 1.0 0.0 0.5 
 EOF
