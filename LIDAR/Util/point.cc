0d05917490a2993b696b7ae1a5d33c2775139e87
Brent Townshend <bst@tc.com>
2014-06-06 12:07:14 -0700

    Made segment2pt inline to speed up frontend

--- LIDAR/Util/point.cc
+++ LIDAR/Util/point.cc
@@ -19,16 +19,3 @@ std::istream& operator>>(std::istream &s,  Point &p) {
 }
 
 
-// Calculate distance from a line segment define by two points and another point
-float  segment2pt(const Point &l1, const Point &l2, const Point &p) {
-    Point D=l2-l1;
-    Point p1=p-l1;
-    float u=D.dot(p)/D.dot(D);
-    if (u<=0)
-	return p1.norm();
-    else if (u>=1)
-	return (p-l2).norm();
-    else 
-	return (p1-D*u).norm();
-}
-
--- LIDAR/Util/point.h
+++ LIDAR/Util/point.h
@@ -70,4 +70,16 @@ inline std::ostream& operator<< (std::ostream& os, const std::vector<float>& v)
     return os;
 }
 
-float  segment2pt(const Point &l1, const Point &l2, const Point &p);
+// Calculate distance from a line segment define by two points and another point
+inline float  segment2pt(const Point &l1, const Point &l2, const Point &p) {
+    Point D=l2-l1;
+    Point p1=p-l1;
+    float u=D.dot(p)/D.dot(D);
+    if (u<=0)
+	return p1.norm();
+    else if (u>=1)
+	return (p-l2).norm();
+    else 
+	return (p1-D*u).norm();
+}
+
