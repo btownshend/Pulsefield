b3dac4bfe4116e01cf466b5b4ff2186dd4562b63
Brent Townshend <bst@tc.com>
2014-07-05 10:29:37 -0700

    Revamped VisualizerGrid to handle control of Ableton, use regular 8x8 grid

--- Video/Tracker/src/VisualizerGrid.java
+++ Video/Tracker/src/VisualizerGrid.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import oscP5.OscMessage;
 import processing.core.PApplet;
@@ -6,42 +8,87 @@ import processing.core.PConstants;
 import processing.core.PVector;
 
 public class VisualizerGrid extends VisualizerPS {
-	HashMap<Integer,String> gridValues;
+	HashMap<Integer,Integer> assignments;
 	HashMap<Integer,String> gridColors;
 	float gposx[], gposy[];
 	float gridwidth, gridheight;
-	String song;
+	int ncell;
+	String songs[]={"QU","DB","NG","FI","FO","GA","MB","EP","OL","PR"};
+	int song=0;
+	TrackSet ts;
 	
 	VisualizerGrid(PApplet parent) {
 		super(parent);
-		gridValues = new HashMap<Integer,String>();
+		assignments = new HashMap<Integer,Integer>();
 		gridColors = new HashMap<Integer,String>();
-		gposx=new float[60];
-		gposy=new float[60];
-		for (int i=0;i<60;i++) {
-			int row,col;
-			if (i<5) {
-				row=0;
-				col=i+2;
-			} else if (i<12) {
-				row=1;
-				col=i-5+1;		
-			} else if (i<48) {
-				row=(i+6)/9;
-				col=(i-12)%9;
-			} else if (i<55) {
-				row=6;
-				col=i-48+1;
-			} else {
-				row=7;
-				col=i-55+2;
+		int nrow=8;
+		int ncol=8;
+		ncell=nrow*ncol;
+		
+		gposx=new float[ncell];
+		gposy=new float[ncell];
+		for (int r=0;r<nrow;r++) {
+			for (int c=0;c<ncol;c++) {
+				int cell=r*ncol+c;
+				gposx[cell]=(2.0f*c+1)/ncol-1;
+				gposy[cell]=(2.0f*r+1)/nrow-1;
 			}
-			//PApplet.println("Grid "+i+" at row="+row+", col="+col);
-			gposx[i]=(1+2*col)/18f;
-			gposy[i]=1-(1+2*row)/16f;
 		}
-		gridwidth=1f/9;
-		gridheight=1f/8;
+		gridwidth=2f/ncol;
+		gridheight=2f/nrow;
+		song=0;
+		ts=Ableton.getInstance().setTrackSet(songs[song]);
+	}
+	public void start() {
+		song=(song+1)%songs.length;
+		ts=Ableton.getInstance().setTrackSet(songs[song]);
+		PApplet.println("Starting grid with song "+song+": "+ts.name);
+	}
+	public void stop() {
+		assignments.clear();
+		}
+
+	public void update(PApplet parent, Positions allpos) {
+		super.update(parent,allpos);
+	//	HashMap<Integer,Integer> newAssignments=new HashMap<Integer,Integer>();
+		for (Position pos: allpos.positions.values()) {
+			//PApplet.println("ID "+pos.id+" pos="+pos.origin);
+			// Find closest grid position
+			int closest=-1;
+			int current=-1;
+			double mindist=1e10f;
+			// Check if we already had one
+			if (assignments.containsKey(pos.id)) {
+				current=assignments.get(pos.id);
+				closest=current;
+				mindist=(Math.pow(gposx[closest]-pos.origin.x,2)+Math.pow(gposy[closest]-pos.origin.y,2))*0.5*0.5;  // Make it appear a little closer to create hysteresis
+				//PApplet.println("Had existing grid "+closest+" at distance "+Math.sqrt(mindist));	
+			}
+			for (int i=0;i<ncell;i++) {
+				double dist2=Math.pow(gposx[i]-pos.origin.x,2)+Math.pow(gposy[i]-pos.origin.y,2);
+				if (dist2 < mindist) {
+					mindist=dist2;
+					closest=i;
+				}
+			}
+			//PApplet.println("ID "+pos.id+" closest to grid ("+gposx[closest]+","+gposy[closest]+"), position "+closest+" at a distance of "+Math.sqrt(mindist));
+			if (current!=closest) {
+				int track=pos.id%(ts.numTracks)+ts.firstTrack;
+				if (current!=-1)
+					Ableton.getInstance().stopClip(track, current);
+				assignments.put(pos.id,closest);
+				Ableton.getInstance().playClip(track,closest);
+			}
+		}
+
+		Iterator<Map.Entry<Integer,Integer> > i = assignments.entrySet().iterator();
+		while (i.hasNext()) {
+			int id=i.next().getKey();
+			if (!allpos.positions.containsKey(id)) {
+				PApplet.println("update: no update info for assignment for id "+id);
+				i.remove();
+			}
+		}
 	}
 
 	public void draw(PApplet parent, Positions p, PVector wsize) {
@@ -53,20 +100,21 @@ public class VisualizerGrid extends VisualizerPS {
 
 		parent.textSize(16);
 		parent.textAlign(PConstants.CENTER,PConstants.CENTER);
-		for (int cell: gridValues.keySet()) {
-			//PApplet.println("grid "+cell+" = "+gridValues.get(cell)+" "+gridColors.get(cell)+" pos=("+gposx[cell]+","+gposy[cell]+")");
+		for (Map.Entry<Integer,Integer> entry: assignments. entrySet()) {
+			int id=entry.getKey();
+			int cell=entry.getValue();
+			//PApplet.println("grid "+cell+", id="+id+" "+gridColors.get(cell));
 			parent.fill(127,0,0,127);
 			parent.strokeWeight(5);
 			parent.stroke(127,0,0);
-			parent.rect(wsize.x*(gposx[cell]-gridwidth/2),wsize.y*(gposy[cell]-gridheight/2),wsize.x*gridwidth,wsize.y*gridheight);
+			parent.rect(wsize.x*(gposx[cell]-gridwidth/2+1)/2,wsize.y*(gposy[cell]-gridheight/2+1)/2,wsize.x*gridwidth/2,wsize.y*gridheight/2);
 			parent.fill(255);
-			parent.text(gridValues.get(cell),wsize.x*(gposx[cell]-gridwidth/2),wsize.y*(gposy[cell]-gridheight/2),wsize.x*gridwidth,wsize.y*gridheight);
+			parent.text("C"+cell+"-P"+id,wsize.x*(gposx[cell]-gridwidth/2+1)/2,wsize.y*(gposy[cell]-gridheight/2+1)/2,wsize.x*gridwidth/2,wsize.y*gridheight/2);
 		}
 		parent.fill(127);
 		parent.textAlign(PConstants.LEFT, PConstants.TOP);
 		parent.textSize(24);
-		if (song!=null)
-			parent.text(song,5,5);
+		parent.text(ts.name,5,5);
 	}
 
 	public void setnpeople(int n) {
@@ -89,10 +137,10 @@ public class VisualizerGrid extends VisualizerPS {
 				String value=theOscMessage.get(0).stringValue();
 				//PApplet.println("cell "+cell+" = "+value);
 				if (value.isEmpty()) {
-					gridValues.remove(cell);
+					assignments.remove(cell);
 					gridColors.remove(cell);
 				} else
-					gridValues.put(cell,value);
+					assignments.put(cell,Integer.parseInt(value));
 				handled=true;
 			} else if (components.length ==5 && components[4].equals("color")) {
 				String color=theOscMessage.get(0).stringValue();
@@ -100,7 +148,7 @@ public class VisualizerGrid extends VisualizerPS {
 				handled=true;
 			}
 		} else if (components[1].equals("grid") && components[2].equals("song")) {
-			song=theOscMessage.get(0).stringValue();
+			ts=Ableton.getInstance().setTrackSet(theOscMessage.get(0).stringValue());
 			handled=true;
 		}
 		if (!handled)
