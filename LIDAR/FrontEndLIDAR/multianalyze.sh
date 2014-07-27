010e7d9f1951dc6abb8b28285fba61afde07886a
Brent Townshend <bst@tc.com>
2014-07-26 21:00:31 -0700

    Just use first 3 multi for testing

--- LIDAR/FrontEndLIDAR/multianalyze.sh
+++ LIDAR/FrontEndLIDAR/multianalyze.sh
@@ -2,8 +2,9 @@
 # Analyze error performance by looking at multi1..4
 # Use track in multi1 as ground truth and compare with others
 # Analyze in matlab using multicompare
-for i in 1 2 3 4 5 6 7 8
+for i in 1 2 3 # 4 5 6 7 8
 do
    ./frontend -p ../Recordings/multi${i}.ferec -x10 -B 8 -m 2000
 done
 echo Run multitest in matlab
+
