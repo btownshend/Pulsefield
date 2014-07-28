f675a9297c816117653381cb5ea98e82d475e07e
Brent Townshend <bst@tc.com>
2014-07-28 00:41:27 -0700

    Updated template

--- Video/Tracker/src/VisualizerTemplate.java
+++ Video/Tracker/src/VisualizerTemplate.java
@@ -10,10 +10,24 @@ public class VisualizerTemplate extends Visualizer {
 		super();
 	}
 
+	@Override
+	public void start() {
+		super.start();
+		// Other initialization when this app becomes active
+	}
+	
+	@Override
+	public void stop() {
+		super.stop();
+		// When this app is deactivated
+	}
+
+	@Override
 	public void update(PApplet parent, Positions p) {
 		// Update internal state
 	}
 
+	@Override
 	public void draw(PApplet parent, Positions p, PVector wsize) {
 		PGL pgl=((PGraphicsOpenGL)parent.g).pgl;
 		pgl.blendFunc(PGL.SRC_ALPHA, PGL.DST_ALPHA);
@@ -30,5 +44,16 @@ public class VisualizerTemplate extends Visualizer {
 
 		// Add drawing code here
 	}
+	
+	@Override
+	public void drawLaser(PApplet parent, Positions p) {
+//		Laser laser=Laser.getInstance();
+//		laser.bgBegin();   // Start a background drawing
+//		for (Position ps: p.positions.values()) {  
+//			laser.cellBegin(ps.id); // Start a cell-specific drawing
+//			Laser drawing code
+//			laser.cellEnd(ps.id);
+//		}
+//		laser.bgEnd();
+	}
 }
-
