bcb6c93684283e62db7cbe8e8bc5d2a448d6b2f3
Brent Townshend <bst@tc.com>
2014-08-16 09:16:12 -0700

    Added ToastingCow002.svg to VisualizerCows

--- Video/Tracker/src/VisualizerCows.java
+++ Video/Tracker/src/VisualizerCows.java
@@ -2,7 +2,7 @@ import processing.core.PApplet;
 
 
 public class VisualizerCows extends VisualizerIcon {
-	final String cowIcons[]={"P1.svg","P2.svg"};
+	final String cowIcons[]={"P1.svg","P2.svg","ToastingCow002.svg"};
 	VisualizerCows(PApplet parent) {
 		super(parent);
 		setIcons(parent,cowIcons);
