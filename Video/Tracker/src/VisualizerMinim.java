525647ad1467ed61faf2eecb9d92b5e141d4789f
Brent Townshend <bst@tc.com>
2014-08-18 21:45:53 -0700

    Do minim initialization once

--- Video/Tracker/src/VisualizerMinim.java
+++ Video/Tracker/src/VisualizerMinim.java
@@ -13,12 +13,18 @@ public class VisualizerMinim extends VisualizerGrid {
 	AudioRenderer radar, vortex, iso;
 	AudioRenderer[] visuals; 
 	int select;
-	final int numRenderers=3;
 	
 	VisualizerMinim(PApplet parent) {
-		select=0;			// activate first renderer in list
-		minim=null; 	// Will be initialized in update() when someone enters
 		super(parent);
+		select=-1;			// activate first renderer in list
+		// setup player
+		minim = new Minim(parent);
+		// setup renderers
+		vortex = new VortexRenderer(parent,minim.getLineIn());
+		radar = new RadarRenderer(parent,minim.getLineIn());
+		iso = new IsometricRenderer(parent,minim.getLineIn());
+
+		visuals = new AudioRenderer[] {radar, vortex, iso};
 	}
 
 	@Override
@@ -26,21 +32,16 @@ public class VisualizerMinim extends VisualizerGrid {
 		PApplet.println("Minim.start");
 		super.start();
 		// Other initialization when this app becomes active
-		select=(select+1)%numRenderers;
+		select=(select+1)%visuals.length;
 		visuals[select].start(parent);
+		minim.getLineIn().addListener(visuals[select]);
 	}
 
 	@Override
 	public void stop(PApplet parent) {
 		PApplet.println("Minim.stop");
 		super.stop();
-		// When this app is deactivated
-//		groove.close();
-		if (minim!=null) {
-			minim.getLineIn().removeListener(visuals[select]);
-			minim.stop();	
-			minim=null;
-		}
+		minim.getLineIn().removeListener(visuals[select]);
 		visuals[select].stop(parent);
 	}
 
@@ -48,29 +49,16 @@ public class VisualizerMinim extends VisualizerGrid {
 	public void update(PApplet parent, People p) {
 		super.update(parent,p);
 //		PApplet.println("Minim.update: minim="+minim+", num people="+p.pmap.size());
-		if (p.pmap.isEmpty() && minim!=null) {
-			minim.stop();
-			minim=null;
-		} else if (!p.pmap.isEmpty() && minim==null) {
-			// setup player
-			minim = new Minim(parent);
-			// setup renderers
-			vortex = new VortexRenderer(parent,minim.getLineIn());
-			radar = new RadarRenderer(parent,minim.getLineIn());
-			iso = new IsometricRenderer(parent,minim.getLineIn());
-
-			visuals = new AudioRenderer[] {radar, vortex, iso};
-			visuals[select].setup(parent);
-			minim.getLineIn().addListener(visuals[select]);
-		}
 	}
 
 	@Override
 	public void draw(PApplet parent, People p, PVector wsize) {
-		if (minim==null || p.pmap.isEmpty()) {
+		if (p.pmap.isEmpty()) {
 			super.draw(parent, p, wsize);
 			return;
 		}
+		assert(minim!=null);
+
 		// Add drawing code here
 		initializeContext(parent);
 		visuals[select].draw(parent);
