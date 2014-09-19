f80eda8338c77db0cef5f6a2303ec8016514cb9e
Brent Townshend <bst@tc.com>
2014-09-18 22:26:54 -0700

    run.sh to use 4 lasers

--- Laser/run.sh
+++ Laser/run.sh
@@ -6,7 +6,7 @@ while true
 do
     LOGFILE=$LOGDIR/$(date +%Y%m%dT%H%M%S)-osclaser.log
     echo Running osclaser with output to $LOGFILE
-    ./osclaser -n2 -D $LOGFILE -d2
+    ./osclaser -n4 -D $LOGFILE -d2
     echo Osclaser exitted, pausing 2 seconds to restart
     sleep 2
 done
