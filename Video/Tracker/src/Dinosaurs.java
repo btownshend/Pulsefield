0ce8b1bc41753f618c054e5e3e745827582a1418
Brent Townshend <bst@tc.com>
2014-10-04 18:13:59 -0700

    Added interface to dinosaur servos

---
+++ Video/Tracker/src/Dinosaurs.java
@@ -0,0 +1,8 @@
+public class Dinosaurs {
+	// Number of dinosaurs
+	int size() { return 0; }
+	// Number of servos on a given dinosaur
+	int numServos(int dinosaur) { return 0; }
+	// Move a servo on the given dinosaur to the specified angle (in degrees), taking 'timeToMove' seconds to get there
+	void setServo(int dinosaur, int servo, float angle, float timeToMove) {; }
+}
