adf02b639b9470991f073b49a6b00f60e2814f43
Brent Townshend <bst@tc.com>
2015-05-14 23:39:01 -0700

    Modified dbg level in run.sh

--- Laser/run.sh
+++ Laser/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# Run conductor
+# Run laser
 LOGDIR=~/Desktop/CRSLogs
 [ -d $LOGDIR ] || mkdir -p $LOGDIR
 while true
 do
     LOGFILE=$LOGDIR/$(date +%Y%m%dT%H%M%S)-osclaser.log
     echo Running osclaser with output to $LOGFILE
-    ./osclaser -n4 -D $LOGFILE -d2
+    ./osclaser -n4 -D $LOGFILE -d1
     echo Osclaser exitted, pausing 2 seconds to restart
     sleep 2
 done
