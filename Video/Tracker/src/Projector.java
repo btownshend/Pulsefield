ad09eceab5f79fc9800fbe05b27dda3ae6b0647d
Brent Townshend <bst@tc.com>
2016-05-25 11:48:06 -0700

    Verify that reconstructed matrix matches expected

--- Video/Tracker/src/Projector.java
+++ Video/Tracker/src/Projector.java
@@ -389,8 +389,23 @@ public class Projector {
 
 		PApplet.println("product:");
 		product.print();
+		float mx=maxdiff(projmodelview,product);
+		if (mx>0.01f)
+			PApplet.println("reconstructed product differs from original by up to "+mx);
 	}
 
+	public float maxdiff(PMatrix3D a, PMatrix3D b){
+		float diffs[]={a.m00-b.m00,a.m01-b.m01,a.m02-b.m02,a.m03-b.m03,
+				a.m10-b.m10,a.m11-b.m11,a.m12-b.m12,a.m13-b.m13,
+				a.m20-b.m20,a.m21-b.m21,a.m22-b.m22,a.m23-b.m23,
+				a.m30-b.m30,a.m31-b.m31,a.m32-b.m32,a.m33-b.m33};	
+		float	mx=0;
+		for (int i=0;i<diffs.length;i++) {
+			mx=Math.max(mx,Math.abs(diffs[i]));
+		}
+		return mx;
+	}
+	
 	public void setInvMatrix(PMatrix3D projmodelview, boolean zknown) {
 		// Decompose to form independent projection and modelview matrices
 		// If zknown is false, then the 3rd row and 3rd column of projmodelview are undetermined 
