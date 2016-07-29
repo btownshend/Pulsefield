15467cf53aacebd9bd6cd906e395f26cd229d7b6
Brent Townshend <bst@tc.com>
2016-07-29 14:45:28 -0700

    Removed unused import

--- Video/Tracker/src/GUI.java
+++ Video/Tracker/src/GUI.java
@@ -9,7 +9,6 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import processing.core.PApplet;
