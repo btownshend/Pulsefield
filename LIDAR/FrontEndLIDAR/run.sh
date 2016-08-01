e43644fd5f8ffbe30ae473282d4ae7c1574a1508
Brent Townshend <bst@tc.com>
2016-07-31 17:14:13 -0700

    Look for libmat in working dir

--- LIDAR/FrontEndLIDAR/run.sh
+++ LIDAR/FrontEndLIDAR/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Run frontend
-export DYLD_LIBRARY_PATH=/Applications/MATLAB/bin/maci64
+export DYLD_LIBRARY_PATH=.
 LOGDIR=~/Desktop/PFLogs
 [ -d $LOGDIR ] || mkdir -p $LOGDIR
 while true
