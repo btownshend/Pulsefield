89f1bd672d22410b069979063f53a857f07237f0
Brent Townshend <bst@tc.com>
2016-08-06 21:58:52 -0700

    Added Soccer goals

--- Video/Tracker/src/VisualizerSoccer.java
+++ Video/Tracker/src/VisualizerSoccer.java
@@ -1,6 +1,7 @@
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PGraphics;
+import processing.core.PImage;
 import processing.core.PShape;
 import processing.core.PVector;
 
@@ -103,11 +104,115 @@ class Ball {
 	}
 }
 
+class Goal {
+	PVector pos;
+	float width, depth;
+	PImage img;
+	PShape outline;
+	
+	Goal(PVector pos, float width, float depth) {
+		this.pos=pos;
+		this.width=width;
+		this.depth=depth;
+		this.img=Tracker.theTracker.loadImage("soccer/goal.jpg");
+		// Vector pointing into goal
+	}
+	
+	void draw(PGraphics g) {
+		PVector aim=PVector.sub(pos,Tracker.getFloorCenter());
+		aim.normalize();
+		
+		g.pushMatrix();
+		g.translate(pos.x, pos.y);
+		g.rotate((float)Math.atan2(aim.y,aim.x));
+		g.tint(255);
+		g.imageMode(PConstants.CENTER);
+		g.image(img,0f,0f,depth,width);
+		//PApplet.println("Drawing image at "+pos+"; width="+width+", depth="+depth);
+		g.popMatrix();
+		
+		g.stroke(0,255,0);
+		g.noFill();
+		
+		outline=g.createShape();
+		outline.noFill();
+		outline.beginShape();
+		outline.vertex(pos.x-depth/2*aim.x-width/2*aim.y, pos.y-depth/2*aim.y-width/2*aim.x);
+		outline.vertex(pos.x+depth/2*aim.x-width/2*aim.y, pos.y+depth/2*aim.y-width/2*aim.x);
+		outline.vertex(pos.x+depth/2*aim.x+width/2*aim.y, pos.y+depth/2*aim.y+width/2*aim.x);
+		outline.vertex(pos.x-depth/2*aim.x+width/2*aim.y, pos.y-depth/2*aim.y+width/2*aim.x);
+		outline.endShape();
+
+		g.shape(outline);
+	}
+	
+	float pt2line(PVector p1, PVector p2, PVector pt) {
+		PVector v1=PVector.sub(pt, p1);
+		PVector delta=PVector.sub(p2, p1);
+		float len=PVector.dot(v1, delta)/delta.magSq();
+		if (len<0)
+			return PVector.sub(pt, p1).mag();
+		else if (len>1)
+			return PVector.sub(pt,p2).mag();
+		else {
+			PVector p=PVector.add(p1, PVector.mult(delta, len)); // Closest pt
+			return PVector.sub(p, pt).mag();
+		}
+	}
+	
+	// Check if ball hits goal and return new velocity if so
+	// return true if this counts as a goal
+	boolean collisionDetect(Ball b) {
+		if (outline==null) {
+			PApplet.println("no outline");
+			return false;
+		}
+			
+		PVector p1=outline.getVertex(0);
+		for (int i=0;i<outline.getVertexCount();i++) {
+			PVector p2=outline.getVertex((i+1)%outline.getVertexCount());
+			float dist=pt2line(p1,p2,b.position);
+			if (dist<b.radius) {
+				float dist2=pt2line(p1,p2,PVector.add(b.position, PVector.mult(b.velocity,1e-5f)));
+				// Check if its getting closer
+				if (dist2<dist) {
+					PApplet.println("ball at "+b.position+" hit goal line "+i+" between "+p1+" and "+p2);
+					PApplet.println("dist="+dist+", dist2="+dist);
+					if (i==outline.getVertexCount()-1) {
+						// Through the goal line
+						PApplet.println("Goal!");
+						b.impactSound(3);
+						return true;
+					}
+					// Resolve velocity into parallel and perp components
+					PVector parallel=PVector.sub(p2,p1);
+					parallel.normalize();
+					parallel.mult(b.velocity.dot(parallel));
+					PVector perp=PVector.sub(b.velocity, parallel);
+					PApplet.println("vel="+b.velocity+"="+parallel+"(para) + "+perp);
+					b.velocity = PVector.sub(parallel,perp);  // Bounce
+					b.impactSound(2);
+				}
+			}
+			p1=p2;
+		}
+		return false;
+	}
+}
+
 public class VisualizerSoccer extends VisualizerDot {
 	Ball ball;
+	Goal goals[];
+	int score[];
 	
 	VisualizerSoccer(PApplet parent) {
 		super(parent);
+		goals=new Goal[2];
+		goals[0]=new Goal(new PVector(Tracker.getFloorCenter().x+Tracker.getFloorSize().x*0.35f,Tracker.getFloorCenter().y),
+				1.5f,0.5f);
+		goals[1]=new Goal(new PVector(Tracker.getFloorCenter().x-Tracker.getFloorSize().x*0.35f,Tracker.getFloorCenter().y),
+				1.5f,0.5f);
+		score=new int[2];
 	}
 
 	@Override
@@ -118,6 +223,8 @@ public class VisualizerSoccer extends VisualizerDot {
 		// Other initialization when this app becomes active
 		Ableton.getInstance().setTrackSet("Soccer");
 		ball=null;
+		score[0]=0;
+		score[1]=0;
 	}
 	
 	@Override
@@ -136,6 +243,14 @@ public class VisualizerSoccer extends VisualizerDot {
 		for (Person ps: p.pmap.values()) {  
 			ball.collisionDetect(ps);
 		}
+		for (int i=0;i<goals.length;i++) {
+			if (goals[i].collisionDetect(ball)) {
+				score[i]+=1;
+				ball.position=Tracker.getFloorCenter();
+				ball.velocity=new PVector(0,0);
+			}
+	
+		}
 	}
 
 	@Override
@@ -143,8 +258,14 @@ public class VisualizerSoccer extends VisualizerDot {
 		super.draw(t, g, p);
 		if (p.pmap.isEmpty())
 			return;
+		for (Goal goal: goals)
+			goal.draw(g);
 		if (ball!=null)
 			ball.draw(g);
+		g.textAlign(PConstants.CENTER,PConstants.CENTER);
+		for (int i=0;i<score.length;i++) {
+			drawText(g,0.3f,String.format("%d",score[i]), Tracker.getFloorCenter().x-1+2*i,Tracker.getFloorCenter().y);
+		}
 	}
 	
 	@Override
