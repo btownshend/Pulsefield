75cb90e1ac8c5f6a63da0a3563f45d955bd9d463
Brent Townshend <bst@tc.com>
2017-01-20 17:44:38 -0800

    Added VisualizerJNI

--- Video/Tracker/src/Tracker.java
+++ Video/Tracker/src/Tracker.java
@@ -215,6 +215,7 @@ public class Tracker extends PApplet {
 		Scale scale=new Scale("Major","C");
 		addVis("Pads",new VisualizerPads(this, synth),false);
 		addVis("Navier",visNavier=new VisualizerNavier(this,synth),true); 
+		//addVis("JNI",new VisualizerJNI(this),true); 
 		addVis("Tron",new VisualizerTron(this,scale,synth),true);
 		addVis("Grid",visAbleton=new VisualizerGrid(this),true);
 		oscP5.plug(visAbleton,  "songIncr", "/touchosc/song/incr");
