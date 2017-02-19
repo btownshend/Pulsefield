7e9033259efeef2947104f6d8a40e404ea0034e1
Brent Townshend <bst@tc.com>
2017-02-18 23:52:00 -0800

    Clean up indentation

--- Video/Tracker/OF/GPUFluids/src/ofApp.h
+++ Video/Tracker/OF/GPUFluids/src/ofApp.h
@@ -6,39 +6,39 @@
 #include "ofxOsc.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
- private:
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+private:
     vector<punctualForce> pendingForces;
-		void updateFlame();
-    	ofxSyphonServer syphon[5];
+    void updateFlame();
+    ofxSyphonServer syphon[5];
     void saveTexture(string filename, const ofTexture &tex);
     
     // ofxFluid stuff
     ofxFluid fluid;
     
-    ofVec2f oldM;
+    ofVec2f oldM;   // Prior mouse position
     int     width,height;
     
     // ofxOsc stuff
     ofxOscReceiver receiver;
-
+    
     // Flame parameters
     Boolean flameEnable;
     ofPoint flamePosition,flameVelocity;
