f2666c4571e7d0a882e1795b86b397cdf5f6674a
Brent Townshend <bst@tc.com>
2017-04-08 17:38:11 -0700

    Access SVGFiles in data/

--- modules/Tracker/src/Tracker.java
+++ modules/Tracker/src/Tracker.java
@@ -21,7 +21,7 @@ public class Tracker extends PApplet {
 	/**
 	 * 
 	 */
-	public static final String SVGDIRECTORY="../../../SVGFiles/";
+	public static final String SVGDIRECTORY="SVGFiles/";
 	private static boolean present = false;
 	private static boolean autocycle = false;
 	private static boolean starting = true;   // Disable bad OSC messages before setup
