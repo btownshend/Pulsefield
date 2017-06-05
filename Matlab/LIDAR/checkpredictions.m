c8b348fbc58d987cd7e980d5311361bcc287aee0
Brent Townshend <bst@tc.com>
2017-06-04 17:02:33 -0700

    Updated checkpredictions.m

--- Matlab/LIDAR/checkpredictions.m
+++ Matlab/LIDAR/checkpredictions.m
@@ -4,17 +4,22 @@ function checkpredictions(csnap)
   for i=1:length(csnap)
     for j=1:length(csnap(i).tracker.tracks)
       t=csnap(i).tracker.tracks(j);
-      data{t.id}(t.age)=t;
+      if t.trackedBy == csnap(i).vis.unit
+        data{t.id}(i)=t;
+      end
     end
   end
   minscanpts=4;
   z=[];p=[];
   for i=1:length(data)
     prederr=[]; zoherr=[]; scanpts=[];frameid=[];
-    for j=20:length(data{i})
+    for j=1:length(data{i})
+      if isempty(data{i}(j).id) || data{i}(j).age<20
+        continue;
+      end
       t=data{i}(j);
       prederr(end+1,:,:)=t.legs-t.predictedlegs;
-      zoherr(end+1,:,:)=t.legs-data{i}(j-1).legs;
+      zoherr(end+1,:,:)=t.legs-t.prevlegs;
       scanpts(end+1,:)=[length(t.scanpts{1}),length(t.scanpts{2})];
       frameid(end+1,:)=[i,j];
     end
@@ -33,18 +38,18 @@ function checkpredictions(csnap)
   
   setfig('predictions');clf;
   subplot(211);
-  pdfplot(p(:));
+  pdfplot(p(p(:)~=0));
   hold on;
-  pdfplot(z(:));
+  pdfplot(z(z(:)~=0));
   c=axis;axis([-0.1 0.1 0 c(4)]);
   title('Prediction Error');
   xlabel('Error');
   legend('Predicted','ZOH');
 
   subplot(212);
-  cdfplot(abs(p(:)));
+  cdfplot(abs(p(p(:)~=0)));
   hold on; 
-  cdfplot(abs(z(:)));
+  cdfplot(abs(z(z(:)~=0)));
   xlabel('Error');
   cut=prctile(z(:),99);
   axis([0 0.05 0 1]);
