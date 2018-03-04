79f7d497e6da35b7c04eb9f12409e6d166c59fc0
Brent Townshend <bst@tc.com>
2018-03-03 16:57:43 -0800

    Turn off T* numbers in Voronoi

--- modules/Tracker/src/com/pulsefield/tracker/VisualizerVoronoi.java
+++ modules/Tracker/src/com/pulsefield/tracker/VisualizerVoronoi.java
@@ -205,7 +205,7 @@ public class VisualizerVoronoi extends VisualizerPS {
 			Pnt cc=triangle.getCircumcenter();
 			g.fill(255);
 			g.textAlign(PConstants.CENTER,PConstants.CENTER);
-			drawText(g,0.20f,"T"+tnum,(float)cc.coord(0),(float)cc.coord(1));
+			//drawText(g,0.20f,"T"+tnum,(float)cc.coord(0),(float)cc.coord(1));
 			tnum++;
 			for (Pnt site: triangle) {
 				if (site==null) {
